8e8d24eb90f11fb4b7535d26e434d67207ac01f5
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-21 06:28:04 +0000

    setup記法に書き換え

--- src/resources/js/components/atoms/BookTab.vue
+++ src/resources/js/components/atoms/BookTab.vue
@@ -1,59 +1,52 @@
+<script setup>
+import { ref } from "vue";
+
+const isActive = ref(1);
+
+function isSelect(num) {
+    this.isActive = num;
+}
+
+const props = defineProps({
+    is_comment: Boolean,
+});
+</script>
 <template>
     <div class="tab">
         <ul class="tabMenu">
-            <li @click="isSelect('1')" :class="{ active: isActive === '1' }">
+            <li @click="isSelect(1)" :class="{ active: isActive === 1 }">
                 エピソード
             </li>
-            <li @click="isSelect('2')" :class="{ active: isActive === '2' }">
+            <li @click="isSelect(2)" :class="{ active: isActive === 2 }">
                 あらすじ
             </li>
-            <li @click="isSelect('3')" :class="{ active: isActive === '3' }">
+            <li @click="isSelect(3)" :class="{ active: isActive === 3 }">
                 作品情報
             </li>
             <li
                 v-if="is_comment"
-                @click="isSelect('4')"
-                :class="{ active: isActive === '4' }"
+                @click="isSelect(4)"
+                :class="{ active: isActive === 4 }"
             >
                 コメント
             </li>
         </ul>
         <div class="tabContents">
-            <div v-if="isActive === '1'">
+            <div v-if="isActive === 1">
                 <slot name="episode"></slot>
             </div>
-            <div v-else-if="isActive === '2'">
+            <div v-else-if="isActive === 2">
                 <slot name="story"></slot>
             </div>
-            <div v-else-if="isActive === '3'">
+            <div v-else-if="isActive === 3">
                 <slot name="info"></slot>
             </div>
-            <div v-else-if="isActive === '4'">
+            <div v-else-if="isActive === 4">
                 <slot name="comment"></slot>
             </div>
         </div>
     </div>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            isActive: "1",
-        };
-    },
-    props: {
-        is_comment: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    methods: {
-        isSelect: function (num) {
-            this.isActive = num;
-        },
-    },
-};
-</script>
 <style lang="scss" scoped>
 .tab {
     @apply w-full;
