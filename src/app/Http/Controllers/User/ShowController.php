1f2c6f982e2acc1bb448a16986d645f97c59d198
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-27 13:08:58 +0000

    エラー修正

--- src/app/Http/Controllers/User/ShowController.php
+++ src/app/Http/Controllers/User/ShowController.php
@@ -16,7 +16,10 @@ class ShowController extends Controller
     public function __invoke(string $username)
     {
         $user = User::where('username', $username)->first();
-        $books = $user->books()->latest()->get();
+
+        if ($user->books()) {
+            $books = $user->books()->latest()->get();
+        }
 
         // $user = DB::select("select * from users where username = $username limit 1");
         // $books = DB::select("select * from `books` where `books`.`user_id` = 1 and `books`.`user_id` is not null order by `created_at` desc");
