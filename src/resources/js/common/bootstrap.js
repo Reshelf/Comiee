70c335b46aafd2f8f2f24ddbd05081b78d9de0f8
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-30 01:42:28 +0000

    フロントビルド

--- src/resources/js/common/bootstrap.js
+++ src/resources/js/common/bootstrap.js
@@ -6,7 +6,7 @@ window._ = _;
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 /**
@@ -14,14 +14,17 @@ axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: true,
 });
+
+
+window.csrf_token = "{{ csrf_token() }}";
