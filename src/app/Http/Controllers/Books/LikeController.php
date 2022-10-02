28399130f3381a023a769696180f760ad7d07faf
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-02 14:18:06 +0000

    お気に入りのメールは自分を含まない

--- src/app/Http/Controllers/Books/LikeController.php
+++ src/app/Http/Controllers/Books/LikeController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\books\LikedBookMail;
+
 class LikeController extends Controller
 {
     public function __construct()
@@ -24,6 +27,10 @@ class LikeController extends Controller
         if ($book->user->id !== $request->user()->id) {
             $book->likes()->detach($request->user()->id);
             $book->likes()->attach($request->user()->id);
+
+            // お気に入りされたらメールを送る
+            $email = $book->user->email;
+            Mail::to($email)->send(new LikedBookMail($request->user()));
         }
 
         return [
---
+++ src/app/Mail/books/LikedBookMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail\books;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class LikedBookMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from(env('MAIL_FROM_ADDRESS'))
+            ->view('emails.books.liked')
+            ->subject('作品がお気に入りに登録されました！');
+    }
+}
