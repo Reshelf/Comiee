bdef71b0b47b21d466701053cb05b9fbe2ac0648
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-08 23:23:23 +0900

    フッター修正

--- resources/views/_patials/_footer.blade.php
+++ resources/views/_patials/_footer.blade.php
@@ -2,8 +2,8 @@
     フッター
 </div> --}}
 
-<div class="w-full mt-16 bg-f4 py-16 text-[#232323]">
-    <div class="max-w-5xl mx-auto flex pb-12 border-b border-[#232323]">
+<div class="w-full mt-16 bg-f8 py-16 text-[#7c7c7c]">
+    <div class="max-w-5xl mx-auto flex pb-12 border-b border-[#7c7c7c]">
         <div class="w-2/5 flex flex-col items-start">
             <svg height="40" viewBox="0 0 237 40" fill="none">
                 <path
@@ -43,7 +43,7 @@
                         <svg width="24" height="24" viewBox="0 0 32 32" fill="none">
                             <path
                                 d="M11.7887 28C8.55374 28 5.53817 27.0591 3 25.4356C5.15499 25.5751 8.95807 25.2411 11.3236 22.9848C7.76508 22.8215 6.16026 20.0923 5.95094 18.926C6.25329 19.0426 7.6953 19.1826 8.50934 18.856C4.4159 17.8296 3.78793 14.2373 3.92748 13.141C4.695 13.6775 5.99745 13.8641 6.50913 13.8174C2.69479 11.0882 4.06703 6.98276 4.74151 6.09635C7.47882 9.88867 11.5812 12.0186 16.6564 12.137C16.5607 11.7174 16.5102 11.2804 16.5102 10.8316C16.5102 7.61092 19.1134 5 22.3247 5C24.0025 5 25.5144 5.71275 26.5757 6.85284C27.6969 6.59011 29.3843 5.97507 30.2092 5.4432C29.7934 6.93611 28.4989 8.18149 27.7159 8.64308C27.7224 8.65878 27.7095 8.62731 27.7159 8.64308C28.4037 8.53904 30.2648 8.18137 31 7.68256C30.6364 8.52125 29.264 9.91573 28.1377 10.6964C28.3473 19.9381 21.2765 28 11.7887 28Z"
-                                fill="#3A3A3A" />
+                                fill="#7c7c7c" />
                         </svg>
                     </a>
 
@@ -51,11 +51,11 @@
                     <a href="" target="_blank" rel="noopener noreferrer" class="inline-block mr-5">
                         <svg width="24" height="24" viewBox="0 0 32 32" fill="none">
                             <rect x="2" y="2" width="28" height="28" rx="6"
-                                fill="#3A3A3A" />
+                                fill="#7c7c7c" />
                             <rect x="2" y="2" width="28" height="28" rx="6"
-                                fill="#3A3A3A" />
+                                fill="#7c7c7c" />
                             <rect x="2" y="2" width="28" height="28" rx="6"
-                                fill="#3A3A3A" />
+                                fill="#7c7c7c" />
                             <path
                                 d="M23 10.5C23 11.3284 22.3284 12 21.5 12C20.6716 12 20 11.3284 20 10.5C20 9.67157 20.6716 9 21.5 9C22.3284 9 23 9.67157 23 10.5Z"
                                 fill="white" />
@@ -69,24 +69,24 @@
                                 <radialGradient id="paint0_radial_29_1988" cx="0" cy="0"
                                     r="1" gradientUnits="userSpaceOnUse"
                                     gradientTransform="translate(12 23) rotate(-55.3758) scale(25.5196)">
-                                    <stop stop-color="#3A3A3A" />
-                                    <stop offset="0.79309" stop-color="#3A3A3A" />
-                                    <stop offset="1" stop-color="#3A3A3A" />
+                                    <stop stop-color="#7c7c7c" />
+                                    <stop offset="0.79309" stop-color="#7c7c7c" />
+                                    <stop offset="1" stop-color="#7c7c7c" />
                                 </radialGradient>
                                 <radialGradient id="paint1_radial_29_1988" cx="0" cy="0"
                                     r="1" gradientUnits="userSpaceOnUse"
                                     gradientTransform="translate(11 31) rotate(-65.1363) scale(22.5942)">
-                                    <stop stop-color="#3A3A3A" />
-                                    <stop offset="0.444662" stop-color="#3A3A3A" />
-                                    <stop offset="0.71474" stop-color="#3A3A3A" />
-                                    <stop offset="1" stop-color="#3A3A3A" stop-opacity="0" />
+                                    <stop stop-color="#7c7c7c" />
+                                    <stop offset="0.444662" stop-color="#7c7c7c" />
+                                    <stop offset="0.71474" stop-color="#7c7c7c" />
+                                    <stop offset="1" stop-color="#7c7c7c" stop-opacity="0" />
                                 </radialGradient>
                                 <radialGradient id="paint2_radial_29_1988" cx="0" cy="0"
                                     r="1" gradientUnits="userSpaceOnUse"
                                     gradientTransform="translate(0.500002 3) rotate(-8.1301) scale(38.8909 8.31836)">
-                                    <stop offset="0.156701" stop-color="#3A3A3A" />
-                                    <stop offset="0.467799" stop-color="#3A3A3A" />
-                                    <stop offset="1" stop-color="#3A3A3A" stop-opacity="0" />
+                                    <stop offset="0.156701" stop-color="#7c7c7c" />
+                                    <stop offset="0.467799" stop-color="#7c7c7c" />
+                                    <stop offset="1" stop-color="#7c7c7c" stop-opacity="0" />
                                 </radialGradient>
                             </defs>
                         </svg>
@@ -96,7 +96,7 @@
                         <svg width="24" height="24" viewBox="0 0 32 32" fill="none">
                             <path
                                 d="M2.24451 9.94111C2.37304 7.96233 3.96395 6.41157 5.94447 6.31345C8.81239 6.17136 12.9115 6 16 6C19.0885 6 23.1876 6.17136 26.0555 6.31345C28.0361 6.41157 29.627 7.96233 29.7555 9.94111C29.8786 11.8369 30 14.1697 30 16C30 17.8303 29.8786 20.1631 29.7555 22.0589C29.627 24.0377 28.0361 25.5884 26.0555 25.6866C23.1876 25.8286 19.0885 26 16 26C12.9115 26 8.81239 25.8286 5.94447 25.6866C3.96395 25.5884 2.37304 24.0377 2.24451 22.0589C2.12136 20.1631 2 17.8303 2 16C2 14.1697 2.12136 11.8369 2.24451 9.94111Z"
-                                fill="#3A3A3A" />
+                                fill="#7c7c7c" />
                             <path d="M13 12V20L21 16L13 12Z" fill="white" />
                         </svg>
                     </a>
