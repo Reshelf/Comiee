4384d2064d0f42b1f665ddd75946816c856b0887
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-08 23:37:56 +0900

    修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,7 +50,7 @@ module.exports = {
             'dark-2': '#181A1F',
             'dark-blue': '#022f61',
             //color
-            't-color': '#16184b',
+            't-color': '#7c7c7c',
             't-color-2': '#565659',
             't-color-3': '#8f8f90',
             // 't-color': '#455078',
