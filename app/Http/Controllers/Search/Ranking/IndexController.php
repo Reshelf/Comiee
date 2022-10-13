f4353562eb8eada6c73e4104b2468741c6a2fcd0
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-13 19:14:38 +0900

    ランキング お気に入り数0の作品は除外

--- app/Http/Controllers/Search/Ranking/IndexController.php
+++ app/Http/Controllers/Search/Ranking/IndexController.php
@@ -18,8 +18,14 @@ class IndexController extends Controller
     public function __invoke(Request $request)
     {
         // 人気順
-        $books = Book::withCount('likes')->orderBy('likes_count', 'desc')->paginate(50);
+        $likes = Book::withCount('likes')
+            ->orderBy('likes_count', 'desc')
+            ->take(500)->get();
 
+        // お気に入り数が0の作品は除く
+        $books = $likes->where('likes_count', '>', 0);
+
+        // dd($books);
         return view('search.ranking', compact('books'));
     }
 }
