90ed0b0c094bf8058579956ed96b54b495df81c0
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-17 16:46:59 +0900

    スタイル修正

--- resources/views/search/_patials/_term_tabs.blade.php
+++ resources/views/search/_patials/_term_tabs.blade.php
@@ -3,10 +3,14 @@
         <a href="{{ route('search.ranking') }}"
             class="{{ $ranking ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">総合</a>
         <a href="{{ route('search.todays_new') }}"
-            class="{{ $todays_new ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">デイリー</a>
+            class="{{ $todays_new ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">少年・青年</a>
         <a href="{{ route('search.like') }}"
-            class="{{ $like ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">週間</a>
+            class="{{ $like ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">少女・女性</a>
         <a href="{{ route('search.ranking') }}"
-            class="{{ $ranking ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">月間</a>
+            class="{{ $ranking ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">TL</a>
+        <a href="{{ route('search.todays_new') }}"
+            class="{{ $todays_new ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">BL</a>
+        <a href="{{ route('search.like') }}"
+            class="{{ $like ? 'border-primary text-primary font-bold' : 'border-transparent hover:text-primary hover:font-semibold dark:border-dark' }} text-base py-3 px-6 border-b-2">オトナ</a>
     </div>
 @endif
--- resources/views/search/tag_name.blade.php
+++ resources/views/search/tag_name.blade.php
@@ -11,7 +11,7 @@
     ])
     <div class="relative bg-[#0A2140] dark:bg-dark bg-opacity-50 flex w-full mx-auto py-12 items-center">
         <div class="max-w-6xl w-full mx-auto text-white font-semibold flex items-center">
-            <h2 class="text-3xl">「{{ $tag->hashtag }}」の検索結果</h2>
+            <h2 class="text-3xl">「{{ $tag->hashtag }}」</h2>
             <span class="inline-block text-2xl ml-4">
                 {{ $tag->books->count() }}件
             </span>
