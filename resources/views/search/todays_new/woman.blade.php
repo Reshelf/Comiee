b090b72fd952343a9553b2e5aff6bb7e98e662ca
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-21 22:18:30 +0900

    新作 リファクタリング

--- resources/views/search/todays_new/woman.blade.php
+++ resources/views/search/todays_new/woman.blade.php
@@ -28,22 +28,9 @@
                                     @include('search._patials._filter')
                                     <form class="acd-content" method="POST" action="{{ route('todays_new.woman.search') }}">
                                         @csrf
-                                        <div class="filters w-1/5 flex flex-col pr-12">
-                                            <h4 class="text-xs my-2 py-4 border-b border-ccc">並び替え</h4>
-                                            <input class="visually-hidden" type="radio" name="sort" id="like"
-                                                value="お気に入り数"
-                                                @isset($sort)
-                                                    @if ($sort === 'お気に入り数') checked @endif
-                                                @endisset />
-                                            <label for="like" class="mt-4">お気に入り数</label>
-                                            <input class="visually-hidden" type="radio" name="sort" id="view"
-                                                value="閲覧回数"
-                                                @isset($sort)
-                                                    @if ($sort === '閲覧回数') checked @endif
-                                                @endisset />
-                                            <label for="view" class="mt-4">閲覧回数</label>
-                                        </div>
-                                        <button type="submit" class="btn-border mt-6">絞り込む</button>
+                                        @include('search.todays_new._form', [
+                                            'sort' => $sort,
+                                        ])
                                     </form>
                                 </div>
                             @endisset
