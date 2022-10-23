0107d1adaf59155bca73016ec9f1bc5cbd575dee
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-23 17:03:36 +0900

    トップ sp修正

--- resources/views/atoms/_nav.blade.php
+++ resources/views/atoms/_nav.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white dark:bg-dark w-full flex-none border-b border-ddd dark:border-dark">
+<div class="bg-white dark:bg-dark w-full flex-none lg:border-b border-ddd dark:border-dark">
     <div class="max-w-8xl mx-auto">
-        <div class="py-4 md:border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+        <div class="py-4 lg:px-8 lg:border-0 mx-4 lg:mx-0">
             <div class="relative flex items-center">
 
                 {{-- ロゴ --}}
