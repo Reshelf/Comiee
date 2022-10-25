5c4f3917ea438eb54d3761299d472b659b2422a8
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-26 01:43:22 +0900

    ご利用ガイドスタイル修正

--- resources/views/others/faq/1.blade.php
+++ resources/views/others/faq/1.blade.php
@@ -10,7 +10,7 @@
             @include('others.faq.atoms.left_nav')
         </div>
 
-        <div class="w-[70%] pl-20 pr-48">
+        <div class="w-[70%] py-8 pl-20 pr-48">
             <h2 class="text-3xl font-semibold tracking-widest">サービスについて</h2>
             <span class="inline-block text-xs mt-3">2022/10/17</span>
 
