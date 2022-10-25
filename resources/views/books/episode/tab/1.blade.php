0103b7d9418eba4b40cbe8322096f2f1d313ff98
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-25 11:03:20 +0900

    エピソード forの変数名変更しておく

--- resources/views/books/episode/tab/1.blade.php
+++ resources/views/books/episode/tab/1.blade.php
@@ -11,10 +11,10 @@
                 </form>
             </episode-list>
         @endif
-        @foreach ($book->episodes as $episode)
+        @foreach ($book->episodes as $e)
             <div
                 class="hover:bg-f5 dark:hover:bg-dark-1 my-2 py-2 border-b border-ddd dark:border-dark-1 flex items-center justify-between w-full overflow-hidden rounded-[3px]">
-                <a href="{{ route('book.episode.show', ['book_id' => $book->id, 'episode_number' => $episode->number]) }}"
+                <a href="{{ route('book.episode.show', ['book_id' => $book->id, 'episode_number' => $e->number]) }}"
                     class="flex items-center w-full cursor-pointer">
                     @empty($book->thumbnail)
                         <img src="/img/bg.svg" alt="thumbnail" class="block dark:hidden w-[160px] h-[80px] object-cover">
@@ -29,7 +29,7 @@
                     <div class="w-full flex flex-col px-4">
                         {{-- 日付 --}}
                         <div class="text-666 text-xs">
-                            {{ $episode->created_at->format('Y/m/d') }}
+                            {{ $e->created_at->format('Y/m/d') }}
                         </div>
 
 
@@ -37,23 +37,23 @@
                             {{-- 話数 --}}
                             {{-- 既読 --}}
                             <div class="flex flex-col">
-                                <span class="">第{{ $episode->number }}話</span>
+                                <span class="">第{{ $e->number }}話</span>
 
                                 <div class="flex items-center mt-1">
                                     {{-- 値段 --}}
-                                    @if ($episode->is_free)
+                                    @if ($e->is_free)
                                         <span
                                             class="text-xs bg-[#E50111] dark:bg-opacity-50 dark:text-ccc text-white py-0.5 px-1.5 rounded-[3px]">
                                             無料
                                         </span>
                                     @else
                                         <span class="inline-block ml-2 text-xs bg-eee py-0.5 px-1.5 rounded-[3px]">
-                                            {{ $episode->price }}pt
+                                            {{ $e->price }}pt
                                         </span>
                                     @endif
                                     @auth
                                         @if ($book->user->id !== Auth::user()->id)
-                                            @if ($episode->isReadBy(Auth::user()))
+                                            @if ($e->isReadBy(Auth::user()))
                                                 <span class="inline-block text-xs text-666 ml-2">
                                                     既読
                                                 </span>
@@ -77,7 +77,7 @@
                 <div class="flex items-center pr-4">
                     @if (Auth::id() === $book->user_id)
                         <div class="flex items-center">
-                            {{-- <a href="{{ route('book.episode.edit', ['book_id' => $book->id, 'episode_id' => $episode->id]) }}"
+                            {{-- <a href="{{ route('book.episode.edit', ['book_id' => $book->id, 'episode_id' => $e->id]) }}"
                                                         class="mr-2">
                                                         <svg class="h-5 w-5 cursor-pointer hover:text-primary"
                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor"
@@ -88,7 +88,7 @@
                                                     </a> --}}
                             <delete-modal>
                                 <form method="POST"
-                                    action="{{ route('book.episode.destroy', ['book_id' => $book->id, 'episode_id' => $episode->id]) }}"
+                                    action="{{ route('book.episode.destroy', ['book_id' => $book->id, 'episode_id' => $e->id]) }}"
                                     class="p-2 rounded">
                                     @csrf
                                     @method('DELETE')
