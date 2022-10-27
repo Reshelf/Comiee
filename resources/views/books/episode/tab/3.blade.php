522d44c291733844ffc99afbc33144599b7a5206
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-27 16:40:31 +0900

    コメント修正

--- resources/views/books/episode/tab/3.blade.php
+++ resources/views/books/episode/tab/3.blade.php
@@ -5,15 +5,15 @@
 
         @if (count((array) $episode->comments))
             @foreach ($episode->comments as $comment)
-                @empty($comment->parent_id)
-                    <div id="comment-episode-{{ $episode->number }}" class="mb-2 pt-2 px-2 pb-2">
-                        @include('books.episode.tab.3.comment')
-                    </div>
-                @endempty
+                <div id="{{ $book->title }}-{{ $episode->number }}-comment-{{ $comment->id }}">
+                    @empty($comment->parent_id)
+                        <div class="mb-2 pt-2 px-2 pb-2">
+                            @include('books.episode.tab.3.comment')
+                        </div>
+                    @endempty
 
-                @empty(!$comment->replies)
                     @include('books.episode.tab.3.commentList', ['comments' => $comment->replies])
-                @endempty
+                </div>
             @endforeach
         @endif
     </div>
--- resources/views/books/episode/tab/3/commentList.blade.php
+++ resources/views/books/episode/tab/3/commentList.blade.php
@@ -1,20 +1,9 @@
- {{-- コメント --}}
- {{-- @empty($comment->parent_id)
-     <div id="comment-episode-{{ $episode->number }}" class="mb-2 pt-2 px-2 pb-2">
+ @foreach ($comments as $comment)
+     <div class="mb-2 pt-2 px-2 pb-2 ml-[56px]">
          @include('books.episode.tab.3.comment')
      </div>
- @endempty --}}
 
- @if (count((array) $comments))
-     @foreach ($comments as $comment)
-         <div class="mb-2 pt-2 px-2 pb-2 ml-[56px]">
-             @include('books.episode.tab.3.comment')
-         </div>
-
-         <div class="mb-2 pt-2 px-2 pb-2 ml-[56px]">
-             @if (count((array) $comment->replies))
-                 @include('books.episode.tab.3.commentList', ['comments' => $comment->replies])
-             @endif
-         </div>
-     @endforeach
- @endif
+     <div class="mb-2 pt-2 px-2 pb-2 ml-[56px]">
+         @include('books.episode.tab.3.commentList', ['comments' => $comment->replies])
+     </div>
+ @endforeach
