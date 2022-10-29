322bb36b5e1d456d80fbe1363b55b61530f24f28
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-29 23:06:03 +0900

    フォロー 通知設定

--- app/Http/Controllers/User/FollowController.php
+++ app/Http/Controllers/User/FollowController.php
@@ -26,7 +26,7 @@ class FollowController extends Controller
      */
     public function __invoke(Request $request, string $username)
     {
-        $user = User::where('username', $username)->first();
+        $user = User::where(['username' => $username])->first();
 
         if ($user->id === $request->user()->id) {
             return abort('404', 'Cannot follow yourself.');
@@ -36,8 +36,10 @@ class FollowController extends Controller
         $request->user()->followings()->attach($user);
 
         // フォローされたらメール通知を送る
-        $email = $user->email;
-        Mail::to($email)->send(new NewFollowedUserMail($request->user()));
+        if ($user->m_notice_2 === 1) {
+            $email = $user->email;
+            Mail::to($email)->send(new NewFollowedUserMail($request->user()));
+        }
 
 
         return ['username' => $username];
