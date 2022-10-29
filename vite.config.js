81709ad8d6931f05b1db5b530aa53e4d3a386da2
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-10-29 23:30:59 +0900

    フロントビルド

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
 import vue from "@vitejs/plugin-vue";
-import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/sass/app.scss", "resources/js/app.js"],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler",
+    },
+  },
 });
