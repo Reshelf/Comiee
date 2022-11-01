aa8be5f8feb658ca7edd9caba178a1f7cccbcb86
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-01 19:11:53 +0900

    SP フッター修正

--- resources/views/atoms/_footer.blade.php
+++ resources/views/atoms/_footer.blade.php
@@ -2,7 +2,7 @@
     フッター
 </div> --}}
 <div class="md:py-16"></div>
-<footer class="w-full bg-f8 dark:bg-dark-1 py-8 md:py-16 text-[#7c7c7c] dark:text-[#939599]">
+<footer class="w-full mb-16 md:mb-0 bg-f8 dark:bg-dark-1 py-8 md:py-16 text-[#7c7c7c] dark:text-[#939599]">
     <div class="max-w-5xl mx-auto flex flex-col md:flex-row pb-8 md:pb-12 px-8 md:px-0 border-b border-[#7c7c7c]">
         <div class="md:w-2/5 flex flex-col items-start">
             <svg class="h-[35px] md:h-[40px]" viewBox="0 0 237 40" fill="none">
