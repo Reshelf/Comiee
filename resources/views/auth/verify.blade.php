13aa8277d6003eb4c4f843c534c7ea07bb51049c
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-02 21:38:21 +0900

    確認メール修正

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,29 +4,25 @@
     @include('atoms._simple_nav')
 
     <div class="mt-8">
-        <div class="max-w-xl mx-auto bg-white rounded shadow">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="font-semibold bg-eee text-lg p-4">{{ __('メールアドレスをご確認ください') }}</div>
+        <div class="max-w-lg mx-4 md:mx-auto bg-white rounded shadow">
+            <div class="text-[#5A5777] dark:text-ddd bg-[#F2F2F2] dark:bg-dark-1 font-semibold text-lg py-3 px-4">
+                {{ __('メールアドレスをご確認ください') }}
+            </div>
 
-                        <div class="p-6">
-                            @if (session('resent'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ __('ご登録いただいたメールアドレスに確認用のリンクをお送りしました。') }}
-                                </div>
-                            @endif
-                            <p class="mb-6">
-                                {{ __('メールをご確認ください。') }} <br>
-                                {{ __('もし確認用メールが送信されていない場合は、下記をクリックしてください。') }}
-                            </p>
-                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                                @csrf
-                                <button type="submit" class="btn-border">{{ __('確認メールを再送信する') }}</button>.
-                            </form>
-                        </div>
+            <div class="p-6 dark:bg-dark-1">
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        {{ __('ご登録いただいたメールアドレスに確認用のリンクをお送りしました。') }}
                     </div>
-                </div>
+                @endif
+                <p class="mb-6">
+                    {{ __('メールをご確認ください。') }} <br>
+                    {{ __('もし確認用メールが送信されていない場合は、下記をクリックしてください。') }}
+                </p>
+                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <button type="submit" class="btn-border">{{ __('確認メールを再送信する') }}</button>.
+                </form>
             </div>
         </div>
     </div>
