a3970b04c0900a6612d70906e6fac9e23b65ba86
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-03 14:36:27 +0900

    認証メール修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,18 +3,14 @@
 
 <head>
     @include('atoms.meta')
-
     @vite('resources/sass/app.scss')
-
     @yield('head')
 </head>
 
 <body>
     <div id="app">
-        {{-- @include('atoms._error_toast') --}}
         @yield('content')
     </div>
-
     @vite('resources/js/app.js')
     @yield('scripts')
 </body>
