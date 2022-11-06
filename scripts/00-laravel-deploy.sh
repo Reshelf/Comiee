ec551836917a9d6970bc4528ad6d1f44261cb522
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-06 21:40:15 +0900

    デプロイコマンド修正

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,7 +4,8 @@ composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
-php artisan config:cache
+php artisan config:clear
+php artisan cache:clear
 
 echo "Caching routes..."
 php artisan route:cache
