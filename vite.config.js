67a961396025a5a2b751102c2693ee5ecd655e4b
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-08 08:27:16 +0900

    フロントビルド

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
   plugins: [
@@ -16,6 +16,7 @@ export default defineConfig({
         },
       },
     }),
+    splitVendorChunkPlugin(),
   ],
   // 本番ビルド用
   resolve: {
