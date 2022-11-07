c7f3071987580606c4938c60390b925c44c2183b
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-07 17:56:00 +0900

    db修正

--- database/migrations/2022_06_22_134442_create_episodes_table.php
+++ database/migrations/2022_06_22_134442_create_episodes_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('episodes', function (Blueprint $table) {
             $table->id();
             $table->unsignedbigInteger('book_id')->comment('作品ID');
-            $table->unsignedInteger('number')->nullable();
-            $table->json('contents')->nullable();
+            $table->unsignedInteger('number')->nullable()->comment('エピソードの話数');
+            $table->json('contents')->nullable()->default('[]')->comment('マンガのコンテンツ');
 
-            $table->boolean('is_free')->default(false);
-            $table->integer('price')->default(50);
-            $table->integer('views')->default(0);
+            $table->boolean('is_free')->default(false)->comment('無料フラグ');
+            $table->integer('price')->default(50)->comment('値段');
+            $table->integer('views')->default(0)->comment('閲覧数');
 
             $table->timestamps();
             $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
