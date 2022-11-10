045a6f12544429830ad82e13dfc85101e821a9d5
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-10 22:15:31 +0900

    フロントビルド

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,75 +2,7 @@ import { createApp } from "vue/dist/vue.esm-bundler";
 import "./common/bootstrap";
 import "./common/theme";
 
-// atoms
-import BasicModal from "./components/atoms/BasicModal.vue";
-import BookTab from "./components/atoms/BookTab.vue";
-import ToastModal from "./components/atoms/ToastModal.vue";
-// auth
-import AuthModal from "./components/auth/AuthModal.vue";
-import BookLike from "./components/books/BookLike.vue";
-// books
-import BookEditModal from "./components/books/BookEditModal.vue";
-import CreateModal from "./components/books/CreateModal.vue";
-import DeleteModal from "./components/books/DeleteModal.vue";
-import EditModal from "./components/books/EditModal.vue";
-import CommentPostModal from "./components/books/episodes/comments/CommentPostModal.vue";
-import EpisodeList from "./components/books/episodes/EpisodeList.vue";
-import EpisodeScreen from "./components/books/episodes/EpisodeScreen.vue";
-import UpdateEpisode from "./components/books/episodes/UpdateEpisode.vue";
-// others
-import BookTagsInput from "./components/BookTagsInput.vue";
-import CountAnimation from "./components/CountAnimation.vue";
-import FollowButton from "./components/FollowButton.vue";
-import HeaderUserModal from "./components/HeaderUserModal.vue";
-import HoverMenu from "./components/HoverMenu.vue";
-// search
-import GenreSearch from "./components/search/GenreSearch.vue";
-import RankingSortModal from "./components/search/RankingSortModal.vue";
-import SearchForm from "./components/search/SearchForm.vue";
-import TagSearchModal from "./components/search/TagSearchModal.vue";
-//
-import ThemeToggle from "./components/ThemeToggle.vue";
-// users
-import AvatarZoom from "./components/users/AvatarZoom.vue";
-import EditUserModal from "./components/users/EditUserModal.vue";
-import FollowModal from "./components/users/FollowModal.vue";
-import ThumbnailZoom from "./components/users/ThumbnailZoom.vue";
-
-import CommentLike from "./components/books/episodes/comments/CommentLike.vue";
-import SettingTab from "./components/users/SettingTab.vue";
-
-const components = {
-  SettingTab,
-  CommentLike,
-  BasicModal,
-  TagSearchModal,
-  GenreSearch,
-  SearchForm,
-  RankingSortModal,
-  BookTab,
-  BookEditModal,
-  CommentPostModal,
-  ToastModal,
-  EpisodeScreen,
-  UpdateEpisode,
-  EpisodeList,
-  ThumbnailZoom,
-  FollowModal,
-  AvatarZoom,
-  AuthModal,
-  DeleteModal,
-  HoverMenu,
-  CreateModal,
-  CountAnimation,
-  HeaderUserModal,
-  ThemeToggle,
-  EditModal,
-  EditUserModal,
-  BookLike,
-  BookTagsInput,
-  FollowButton,
-};
+import components from "./common/components";
 const app = createApp({
   components,
 });
