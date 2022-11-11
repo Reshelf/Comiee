7fae3ac7fa5dcbed9fd79df2cfcb752451401d80
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-11 19:26:28 +0900

    vueバグ修正

--- resources/js/components/users/FollowModal.vue
+++ resources/js/components/users/FollowModal.vue
@@ -1,31 +1,34 @@
-<script setup>
-import { ref } from "vue";
-
-const open = ref(true);
-
-function close() {
-  this.open = false;
-  window.history.back();
-}
-
-/* eslint-disable */
-const props = defineProps({
-  userName: {
-    type: String,
-    default: "",
+<script>
+export default {
+  props: {
+    userName: {
+      type: String,
+      default: "",
+    },
   },
-});
+  data() {
+    return {
+      open: true,
+    };
+  },
+  methods: {
+    close() {
+      this.open = false;
+      window.history.back();
+    },
+  },
+};
 </script>
 <template>
-  <div @click.self="close()">
+  <div @click.self="close">
     <button @click="open = true">
       <slot name="trigger"></slot>
     </button>
     <transition name="modal" appear>
-      <div v-show="open" class="overlay" @click.self="close()">
+      <div v-show="open" class="overlay" @click.self="close">
         <div class="window">
           <div class="header">
-            <button class="close" @click="close()">
+            <button class="close" @click="close">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="#333">
                 <path
                   d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
