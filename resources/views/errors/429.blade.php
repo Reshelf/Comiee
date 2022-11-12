ef8b0cac0d72173bc08006ab4a404e1a14a78ea9
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-12 19:00:19 +0900

    エラーページ修正

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -7,5 +7,5 @@
 @section('error-body')
     <p>短時間に制限以上のリクエストをしています！</p>
     <p>時間をおいて再度操作をお願いします。</p>
-    <a href="/login" class="btn-border my-8">トップページへ</a>
+    <a href="/" class="btn-border my-8">トップページへ</a>
 @endsection
