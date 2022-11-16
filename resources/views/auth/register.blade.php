54405038942bd230e891dcccdb930072bd6dd0d9
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-16 09:49:17 +0900

    スタイル修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @include('atoms._simple_nav')
     <div class="max-w-md m-8 md:mx-auto bg-white rounded border border-eee dark:border-none">
-        <h2 class="text-[#5A5777] dark:text-ddd bg-[#F2F2F2] dark:bg-dark-1 font-semibold text-lg pt-3 px-4 pb-6">
+        <h2 class="text-[#5A5777] dark:text-ddd bg-[#F2F2F2] dark:bg-dark-1 font-semibold text-lg pt-3 p-4 mb-6">
             {{ __('新規登録') }}
         </h2>
 
--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -6,7 +6,7 @@
     @include('atoms._simple_nav')
 
     <div class="max-w-md m-8 md:mx-auto bg-white dark:bg-dark-1 rounded border border-eee dark:border-none">
-        <h2 class="text-[#5A5777] dark:text-ddd bg-[#F2F2F2] dark:bg-dark-1 font-semibold text-lg pt-3 px-4 pb-6">
+        <h2 class="text-[#5A5777] dark:text-ddd bg-[#F2F2F2] dark:bg-dark-1 font-semibold text-lg pt-3 p-4 mb-6">
             {{-- {{ __('ログイン') }} --}}
             403 | Forbidden
         </h2>
