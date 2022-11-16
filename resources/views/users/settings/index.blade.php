d37db4ccbebda5e5ccbfc490ac15b66c1564df8f
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-16 18:01:51 +0900

    SPスタイル修正

--- resources/views/users/settings/index.blade.php
+++ resources/views/users/settings/index.blade.php
@@ -10,20 +10,22 @@
             'settings' => true,
         ])
     </div>
-    <div class="flex max-w-6xl w-full mx-auto mt-4 px-6 md:px-0 justify-center mb-8">
-        <div class="w-full md:my-8 mx-12">
-            <div class="w-full flex">
-                @if (Auth::id() === $user->id)
+
+    @if (Auth::id() === $user->id)
+        <div class="flex max-w-6xl w-full mx-auto mt-4 px-6 md:px-0 justify-center mb-8">
+            <div class="w-full md:my-8 mx-12">
+                <div class="w-full flex">
                     <setting-tab :is-comment="false">
                         @include('users.settings.atoms.1')
                         @include('users.settings.atoms.2')
                         @include('users.settings.atoms.3')
                         @include('users.settings.atoms.4')
                     </setting-tab>
-                @endif
+                </div>
             </div>
         </div>
-    </div>
+    @endif
+
 
     @include('atoms._footer')
 @endsection
