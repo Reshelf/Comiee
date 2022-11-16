dd29bf30c13ae9ec5baa134ea63cd65c76bd6ab7
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-16 16:34:39 +0900

    修正

--- resources/views/emails/others/contact.blade.php
+++ resources/views/emails/others/contact.blade.php
@@ -1,38 +1,8 @@
-<body style="margin:0;padding:0;">
-    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
-        <tr>
-            <td align="center" style="padding:0;">
-                <table role="presentation"
-                    style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
-
-                    @include('emails.atoms.header')
-
-                    <tr>
-                        <td style="padding:36px 30px 42px 30px; background:#fff;">
-                            <table role="presentation"
-                                style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-                                <tr>
-                                    <td style="padding:0 0 20px 0;color:#153643;">
-                                        <p>【メールアドレス】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {{ $user->email }}
-                                        </p>
-                                        <p>【お問い合せ内容】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {!! nl2br($body) !!}
-                                        </p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-
-
-                    @include('emails.atoms.footer')
-                </table>
-            </td>
-        </tr>
-    </table>
-</body>
+<p>【メールアドレス】</p>
+<p style="margin:0 0 12px 0;">
+    {{ $user->email }}
+</p>
+<p>【お問い合せ内容】</p>
+<p style="margin:0 0 12px 0;">
+    {!! nl2br($body) !!}
+</p>
--- resources/views/emails/others/report.blade.php
+++ resources/views/emails/others/report.blade.php
@@ -1,48 +1,16 @@
-<body style="margin:0;padding:0;">
-    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
-        <tr>
-            <td align="center" style="padding:0;">
-                <table role="presentation"
-                    style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
-
-                    @include('emails.atoms.header')
-
-                    <tr>
-                        <td style="padding:36px 30px 42px 30px; background:#fff;">
-                            <table role="presentation"
-                                style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-                                <tr>
-                                    <td style="padding:0 0 20px 0;color:#153643;">
-                                        <p>【通報者】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {{ $reports['user'] }}
-                                        </p>
-                                        <p>【通報内容】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {!! nl2br($reports['body']) !!}
-                                        </p>
-                                        <p>【通報されたユーザー】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {{ $reports['reportedUser'] }}
-                                        </p>
-                                        <p>【通報されたコメント内容】</p>
-                                        <p
-                                            style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Noto Sans JP;">
-                                            {!! nl2br($reports['comment']) !!}
-                                        </p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-
-
-                    @include('emails.atoms.footer')
-                </table>
-            </td>
-        </tr>
-    </table>
-</body>
+<p>【通報者】</p>
+<p style="margin:0 0 12px 0;">
+    {{ $reports['user'] }}
+</p>
+<p>【通報内容】</p>
+<p style="margin:0 0 12px 0;">
+    {!! nl2br($reports['body']) !!}
+</p>
+<p>【通報されたユーザー】</p>
+<p style="margin:0 0 12px 0;">
+    {{ $reports['reportedUser'] }}
+</p>
+<p>【通報されたコメント内容】</p>
+<p style="margin:0 0 12px 0;">
+    {!! nl2br($reports['comment']) !!}
+</p>
