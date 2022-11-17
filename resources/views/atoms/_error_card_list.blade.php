339d3b917e2aa14d69b85c34e64a26c783a27feb
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-18 01:51:48 +0900

    スタイル修正

--- resources/views/atoms/_error_card_list.blade.php
+++ resources/views/atoms/_error_card_list.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
     <ul class="my-4 max-w-6xl mx-auto">
         @foreach ($errors->all() as $error)
-            <li class="text-red bg-red bg-opacity-10 my-2 mx-4 lg:mx-0 font-semibold rounded py-2 px-4">
+            <li class="text-red bg-red bg-opacity-10 my-2 font-semibold rounded py-2 px-4">
                 {{ $error }}
             </li>
         @endforeach
