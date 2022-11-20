24a878c491fbdb315fe0e3ec55568b524ba4f622
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-20 14:10:43 +0900

    バグ解消した

--- app/Http/Controllers/User/UpdateController.php
+++ app/Http/Controllers/User/UpdateController.php
@@ -29,8 +29,8 @@ class UpdateController extends Controller
             'username' => 'required|string|min:4|max:20|regex:/\A([a-zA-Z0-9-_])+\z/u|unique:users,username,' . $user->id . ',id',
             'email' => 'required|email:filter,dns|unique:users,email,' . $user->id . ',id',
             'body' => ['nullable', 'string', 'max:200'],
-            // 'avatar' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp'],
-            // 'thumbnail' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp'],
+            'avatar' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp'],
+            'thumbnail' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp'],
         ]);
 
         $user->name = $request->name;
@@ -40,33 +40,41 @@ class UpdateController extends Controller
         // $user->website = $request->website;
 
         if ($request->has('avatar')) {
-            $avatar =  \Image::make($request->file('avatar'));
-            $avatar->resize(
+            $file = $request->file('avatar');
+            $fileName = $file->getClientOriginalName();
+            $filePath = 'app/users/avatar/' . $fileName;
+
+            $img =  \Image::make($file);
+            $img->resize(
                 800,
                 null,
                 function ($constraint) {
                     $constraint->aspectRatio();
                     $constraint->upsize();
                 }
-            )->limitColors(null)->encode('webp', 0.01)->stream('webp');
+            )->limitColors(null)->encode('webp', 0.01); // 多分最大は0.1
 
-            Storage::disk('s3')->put('app/users/avatar/' . $request->file('avatar')->getClientOriginalName(), $avatar);
-            $user->avatar = Storage::disk('s3')->url('app/users/avatar/' . $request->file('avatar')->getClientOriginalName());
+            Storage::disk('s3')->put($filePath, $img);
+            $user->avatar = Storage::disk('s3')->url($filePath);
         }
 
         if ($request->has('thumbnail')) {
-            $thumbnail =  \Image::make($request->file('thumbnail'));
-            $thumbnail->resize(
+            $file = $request->file('thumbnail');
+            $fileName = $file->getClientOriginalName();
+            $filePath = 'app/users/thumbnail/' . $fileName;
+
+            $img =  \Image::make($file);
+            $img->resize(
                 2000,
                 null,
                 function ($constraint) {
                     $constraint->aspectRatio();
                     $constraint->upsize();
                 }
-            )->limitColors(null)->encode('webp', 0.01)->stream('webp'); // 多分最大は0.1
+            )->limitColors(null)->encode('webp', 0.01); // 多分最大は0.1
 
-            Storage::disk('s3')->put('app/users/thumbnail/' . $request->file('thumbnail')->getClientOriginalName(), $thumbnail);
-            $user->thumbnail = Storage::disk('s3')->url('app/users/thumbnail/' . $request->file('thumbnail')->getClientOriginalName());
+            Storage::disk('s3')->put($filePath, $img);
+            $user->thumbnail = Storage::disk('s3')->url($filePath);
         }
 
         $user->m_notice_1 = false;
