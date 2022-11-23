4ce6e9eab03e5fa96e9190fc8e457c2163339753
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-23 18:47:59 +0900

    修正

--- resources/views/books/episode/show.blade.php
+++ resources/views/books/episode/show.blade.php
@@ -19,7 +19,12 @@
 @endisset
 
 @section('content')
-    @include('atoms._episode_nav', ['tab' => 0])
+    <div class="hidden lg:block">
+        @include('atoms._episode_nav', ['tab' => 0])
+    </div>
+    <div class="block lg:hidden">
+        @include('atoms._nav', ['tab' => 1])
+    </div>
 
     {{-- エピソードスクリーン --}}
     <episode-screen :title='@json($book->title)' :episode-number='@json($episode->number)'
@@ -29,18 +34,21 @@
     <div class="w-full mt-8 lg:mt-auto h-full bg-white dark:bg-dark">
         <div class="max-w-7xl mx-auto md:py-8 flex flex-col md:flex-row justify-between">
             {{-- 左サイドバー --}}
-            @include('books.atoms.leftSidebar')
+            <div class="hidden lg:block">
+                @include('books.atoms.leftSidebar')
+            </div>
 
             <div class="w-full flex py-8">
 
                 {{-- メインコンテンツ --}}
-                <div class="px-4 md:px-6 w-full lg:w-2/3">
+                <div class="hidden lg:block px-4 md:px-6 w-full lg:w-2/3">
                     <book-tab :is-comment="true">
                         @include('books.episode.tab.1')
                         @include('books.episode.tab.2')
                         @include('books.episode.tab.3')
                     </book-tab>
                 </div>
+
 
                 {{-- 右サイドバー --}}
                 <div class="mg:pl-4 lg:w-1/3"></div>
