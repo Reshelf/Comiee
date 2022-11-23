55b025cbd99d995f52962df703d6034889c140dd
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-11-23 18:02:45 +0900

    エピソード修正

--- app/Http/Controllers/Books/Episode/ShowController.php
+++ app/Http/Controllers/Books/Episode/ShowController.php
@@ -14,11 +14,6 @@ use Carbon\Carbon;
 
 class ShowController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /*
     |--------------------------------------------------------------------------
     | エピソードの詳細
