233e5e839cc6d8042ca7ec39124142da7a5c8199
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-02 00:54:05 +0900

    多言語化

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -13,13 +13,13 @@
     <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
         <div class="py-8 w-full">
             <follow-modal :user-name='@json($user->name)'>
-                <template #header>{{ $user->name }}さんのフォロワー</template>
+                <template #header>{{ $user->name }} {{ __('さんのフォロワー') }}</template>
                 @if ($followers->count())
                     @foreach ($followers as $person)
                         @include('users.atoms.person')
                     @endforeach
                 @else
-                    <p>フォロワーはいません</p>
+                    <p>{{ __('フォロワーはいません') }}</p>
                 @endif
             </follow-modal>
         </div>
