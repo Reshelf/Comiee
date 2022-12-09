c068981a4dcb47a89e3257a4a49a9f5a6a852d1f
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-09 11:22:49 +0900

    バグ修正

--- resources/views/books/episode/tab/3.blade.php
+++ resources/views/books/episode/tab/3.blade.php
@@ -3,20 +3,22 @@
 
         @include('books.episode.tab.3.addComment')
 
-        @if (count((array) $comments))
-            @foreach ($comments as $comment)
-                @if ($episode->number === $comment->episode_number)
-                @empty($comment->parent_id)
-                    <div id="{{ $book->title }}-{{ $episode->number }}-comment-{{ $comment->id }}">
-                        <div class="mb-2 pt-2 px-2 pb-2">
-                            @include('books.episode.tab.3.comment')
-                        </div>
+        @isset($comments)
+            @if (count((array) $comments))
+                @foreach ($comments as $comment)
+                    @if ($episode->number === $comment->episode_number)
+                    @empty($comment->parent_id)
+                        <div id="{{ $book->title }}-{{ $episode->number }}-comment-{{ $comment->id }}">
+                            <div class="mb-2 pt-2 px-2 pb-2">
+                                @include('books.episode.tab.3.comment')
+                            </div>
 
-                        @include('books.episode.tab.3.commentList')
-                    </div>
-                @endempty
-            @endif
-        @endforeach
+                            @include('books.episode.tab.3.commentList')
+                        </div>
+                    @endempty
+                @endif
+            @endforeach
+        @endisset
     @endif
 </div>
 </template>
