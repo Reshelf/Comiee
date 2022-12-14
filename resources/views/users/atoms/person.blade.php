2330383be0f2ef07ce2ede32e14753f044aa59d5
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-14 11:44:55 +0900

    バグ修正

--- resources/views/users/atoms/person.blade.php
+++ resources/views/users/atoms/person.blade.php
@@ -1,18 +1,18 @@
 <div class="flex items-center mb-2">
-    <a href="{{ route('users.show', ['lang' => app()->getLocale(), 'username' => $person->username]) }}"
-        class="flex items-center mr-6">
-        @empty($person->avatar)
-            <img src="{{ asset('/img/noimage-user.svg') }}" alt="" class="h-12 w-12 object-cover rounded-full">
-        @else
-            <img src="{{ Auth::user()->avatar }}" alt="avatar" class="h-12 w-12 object-cover rounded-full">
-        @endempty
-        <span class="ml-4 font-semibold truncate max-w-[140px]">{{ $person->name }}</span>
-    </a>
+  <a href="{{ route('users.show', ['lang' => app()->getLocale(), 'username' => $person->username]) }}"
+    class="flex items-center mr-6">
+    @empty($person->avatar)
+      <img src="{{ asset('/img/noimage-user.svg') }}" alt="" class="h-12 w-12 object-cover rounded-full">
+    @else
+      <img src="{{ $person->avatar }}" alt="avatar" class="h-12 w-12 object-cover rounded-full">
+    @endempty
+    <span class="ml-4 font-semibold truncate max-w-[140px]">{{ $person->name }}</span>
+  </a>
 
-    @if (Auth::id() !== $person->id)
-        <follow-button class="ml-auto" :initial-is-followed-by='@json($person->isFollowedBy(Auth::user()))'
-            :authorized='@json(Auth::check())'
-            endpoint="{{ route('users.follow', ['lang' => app()->getLocale(), 'username' => $person->username]) }}">
-        </follow-button>
-    @endif
+  @if (Auth::id() !== $person->id)
+    <follow-button class="ml-auto" :initial-is-followed-by='@json($person->isFollowedBy(Auth::user()))'
+      :authorized='@json(Auth::check())'
+      endpoint="{{ route('users.follow', ['lang' => app()->getLocale(), 'username' => $person->username]) }}">
+    </follow-button>
+  @endif
 </div>
