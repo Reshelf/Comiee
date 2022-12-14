8d81c26a6a499490f7a815eeac56b425e2042dfb
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-14 19:53:18 +0900

    新規登録とパスワード初期化メールの修正

--- resources/views/emails/auth/password_reset.blade.php
+++ resources/views/emails/auth/password_reset.blade.php
@@ -1,2 +1,78 @@
-<p>{{ __('以下のURLをクリックし、パスワードの再設定を行って下さい。') }}</p>
-<p><a href='{{ $restUrl }}'>{{ $restUrl }}</a></p>
+@extends('emails.base')
+
+@section('email-content')
+  {{-- タイトル --}}
+  <tr>
+    <td>
+      <table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse;word-break:break-word">
+        <tbody>
+          <tr>
+            <td>
+              <span
+                style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:16px;line-height:21px;color:#141823">
+                以下のボタンをクリックし、パスワードの再設定を行って下さい。
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td height="28" style="line-height:28px"></td>
+  </tr>
+
+  {{-- 本文 --}}
+  <tr>
+    <td>
+      <table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse">
+        <tbody>
+          <tr>
+            <td
+              style="font-size:11px;font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;background:#ffffff;padding:15px;display:block">
+              <table border="0" width="100%" cellspacing="0" cellpadding="0" align="center"
+                style="border-collapse:collapse;max-width:300px;word-break:break-word">
+                <tbody>
+                  {{-- パスワードの再設定をする --}}
+                  <tr>
+                    <td width="100%" align="center">
+                      <table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse">
+                        <tbody>
+                          <tr>
+                            <td>
+                              <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                style="border-collapse:collapse">
+                                <tbody>
+                                  <tr>
+                                    <td
+                                      style="border-collapse:collapse;border-radius:6px;text-align:center;display:block;background:#1877f2;padding:8px 16px 10px 16px">
+                                      <a href="{{ $restUrl }}"
+                                        style="color:#1b74e4;text-decoration:none;display:block" target="_blank">
+                                        <center>
+                                          <font size="3">
+                                            <span
+                                              style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;white-space:nowrap;font-weight:bold;vertical-align:middle;color:#ffffff;font-weight:500;font-family:Roboto-Medium,Roboto,-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:14px;line-height:14px">
+                                              パスワードの再設定をする
+                                            </span>
+                                          </font>
+                                        </center>
+                                      </a>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </td>
+  </tr>
+@endsection
--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,22 +1,199 @@
-<p>{{ __('Starbookに登録いただきありがとうございます！') }}</p>
+<table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse">
+  <tbody>
+    <tr>
+      <td align="center">
+        <table border="0" cellspacing="0" cellpadding="0" align="center" style="border-collapse:collapse">
+          <tbody>
+            <tr>
+              <td width="1280" align="center">
+                <div style="max-width:640px;margin:0 auto" dir="ltr" bgcolor="#ffffff">
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center"
+                    style="border-collapse:collapse;max-width:640px;margin:0 auto">
+                    <tbody>
+                      <tr>
+                        <td
+                          style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;background:#ffffff">
+                          <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                            style="border-collapse:collapse">
+                            <tbody>
+                              <tr>
+                                <td height="20" style="line-height:20px" colspan="3"></td>
+                              </tr>
 
-@foreach ($introLines as $line)
-    <p>{{ $line }}</p>
-@endforeach
 
-<br>
-<p>{{ $actionUrl }}</p>
-<br>
+                              {{-- アイコン --}}
+                              <tr>
+                                <td>
+                                  <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                    style="border-collapse:collapse">
+                                    <tbody>
+                                      <tr>
+                                        <td height="15" style="line-height:15px" colspan="3"></td>
+                                      </tr>
+                                      <tr>
+                                        <td width="32" align="left" valign="middle"
+                                          style="height:32;line-height:0px">
+                                          <a href="{{ config('app.stripe_url') }}"
+                                            style="color:#1b74e4;text-decoration:none" target="_blank">
+                                            {{-- アイコン --}}
+                                            <img height="32" style="object-fit: cover"
+                                              src="https://starbooks.s3.ap-northeast-1.amazonaws.com/app/system/logo.png"
+                                              alt="starbooks logo">
+                                          </a>
+                                        </td>
+                                      </tr>
+                                      <tr style="border-bottom:solid 1px #e5e5e5">
+                                        <td height="15" style="line-height:15px" colspan="3"></td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </td>
+                              </tr>
 
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-    <p>{{ $line }}</p>
-@endforeach
+
+                              {{-- 内容 --}}
+                              <tr>
+                                <td>
+                                  <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                    style="border-collapse:collapse">
+                                    <tbody>
+                                      <tr>
+                                        <td height="28" style="line-height:28px">
+                                        </td>
+                                      </tr>
+
+                                      {{-- タイトル --}}
+                                      <tr>
+                                        <td>
+                                          <table border="0" cellspacing="0" cellpadding="0"
+                                            style="border-collapse:collapse;word-break:break-word">
+                                            <tbody>
+                                              <tr>
+                                                <td>
+                                                  <span
+                                                    style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:16px;line-height:21px;color:#141823">
+                                                    Starbooksへようこそ！<br>
+                                                    以下のボタンをクリックして、アカウント登録を完了させてください。
+                                                  </span>
+                                                </td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+                                        </td>
+                                      </tr>
+                                      <tr>
+                                        <td height="14" style="line-height:14px"></td>
+                                      </tr>
+
+                                      {{-- 本文 --}}
+                                      <tr>
+                                        <td>
+                                          <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                            style="border-collapse:collapse">
+                                            <tbody>
+                                              <tr>
+                                                <td
+                                                  style="font-size:11px;font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;background:#ffffff;padding:15px;display:block">
+                                                  <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                                    align="center"
+                                                    style="border-collapse:collapse;max-width:300px;word-break:break-word">
+                                                    <tbody>
+                                                      <tr>
+                                                        <td height="7" style="line-height:7px"></td>
+                                                      </tr>
+                                                      <tr>
+                                                        <td height="17" style="line-height:17px"></td>
+                                                      </tr>
+
+                                                      {{-- メール認証を完了する --}}
+                                                      <tr>
+                                                        <td width="100%" align="center">
+                                                          <table border="0" cellspacing="0" cellpadding="0"
+                                                            style="border-collapse:collapse">
+                                                            <tbody>
+                                                              <tr>
+                                                                <td>
+                                                                  <table border="0" width="100%" cellspacing="0"
+                                                                    cellpadding="0" style="border-collapse:collapse">
+                                                                    <tbody>
+                                                                      <tr>
+                                                                        <td
+                                                                          style="border-collapse:collapse;border-radius:6px;text-align:center;display:block;background:#1877f2;padding:8px 16px 10px 16px">
+                                                                          <a href="{{ $actionUrl }}"
+                                                                            style="color:#1b74e4;text-decoration:none;display:block"
+                                                                            target="_blank">
+                                                                            <center>
+                                                                              <font size="3">
+                                                                                <span
+                                                                                  style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;white-space:nowrap;font-weight:bold;vertical-align:middle;color:#ffffff;font-weight:500;font-family:Roboto-Medium,Roboto,-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:14px;line-height:14px">
+                                                                                  アカウント登録を完了する
+                                                                                </span>
+                                                                              </font>
+                                                                            </center>
+                                                                          </a>
+                                                                        </td>
+                                                                      </tr>
+                                                                    </tbody>
+                                                                  </table>
+                                                                </td>
+                                                              </tr>
+                                                            </tbody>
+                                                          </table>
+                                                        </td>
+                                                      </tr>
+                                                    </tbody>
+                                                  </table>
+                                                </td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+                                        </td>
+                                      </tr>
+                                      <tr>
+                                        <td height="28" style="line-height:28px"></td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </td>
+                              </tr>
 
 
-{{-- Salutation --}}
-@if (!empty($salutation))
-    <p> {{ $salutation }}</p>
-@else
-    <p>{{ __('Starbooks運営チーム') }}</p>
-@endif
+                              {{-- 保護 --}}
+                              <tr>
+                                <table border="0" width="100%" cellspacing="0" cellpadding="0"
+                                  style="border-collapse:collapse">
+                                  <tbody>
+                                    <tr style="border-top:solid 1px #e5e5e5">
+                                      <td height="19" style="line-height:19px"></td>
+                                    </tr>
+                                    <tr>
+                                      <td
+                                        style="font-family:Roboto-Regular,Roboto,-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:11px;color:#8a8d91;line-height:16px;font-weight:400;">
+                                        <span
+                                          style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:16px;line-height:21px;color:#141823;font-family:Roboto-Regular,Roboto,-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;font-size:11px;color:#8a8d91;line-height:16px;font-weight:400">
+                                          アカウントを保護するため、このメールを転送しないでください。<br><br>
+                                          Starbooks
+                                        </span>
+                                      </td>
+                                    </tr>
+                                    <tr>
+                                      <td height="20" style="line-height:20px" colspan="3"></td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
