b6a520d6689e64fdfa03710272f3d62985c52cd5
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-16 09:01:37 +0900

    メール修正

--- app/Mail/books/AddNewBookMail.php
+++ app/Mail/books/AddNewBookMail.php
@@ -32,7 +32,7 @@ class AddNewBookMail extends Mailable
         $mailData = $this->mailData;
 
         return $this
-            ->from(env('MAIL_FROM_ADDRESS'))
+            ->from($address = env('MAIL_FROM_ADDRESS'), $name = 'Starbooks Teams')
             ->to($mailData['followers'])
             ->view('emails.books.newBook')
             ->subject($mailData['send_user']->name . 'さんが新しい作品を投稿しました。');
