da22fc6766145ff4d37e807b7c7698e8b65c597f
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-12-31 00:25:51 +0900

    バグ修正

--- resources/views/books/episode/tab/1.blade.php
+++ resources/views/books/episode/tab/1.blade.php
@@ -102,18 +102,20 @@
                     @endauth
 
                     {{-- 決済 --}}
-                    @if ($book->user->id !== Auth::user()->id)
-                      <div class="ml-4">
-                        <button id="checkout-button"
-                          style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em">決済を行う</button>
-                        <div id="error-message"></div>
-                        @include('atoms.stripe_script', [
-                            'book' => $book,
-                            'product' => $e,
-                            'price' => $e->price,
-                        ])
-                      </div>
-                    @endif
+                    @auth
+                      @if ($book->user->id !== Auth::user()->id)
+                        <div class="ml-4">
+                          <button id="checkout-button"
+                            style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em">決済を行う</button>
+                          <div id="error-message"></div>
+                          @include('atoms.stripe_script', [
+                              'book' => $book,
+                              'product' => $e,
+                              'price' => $e->price,
+                          ])
+                        </div>
+                      @endif
+                    @endauth
                   </div>
                 </div>
               </div>
