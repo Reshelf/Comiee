a76f347793fe7d02c45248dd5421408c1d3a25ec
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-01 17:36:38 +0900

    収益設定を済んでいない作者は有料販売をできないようにする

--- resources/views/books/episode/tab/1/episode_form.blade.php
+++ resources/views/books/episode/tab/1/episode_form.blade.php
@@ -47,24 +47,33 @@
 {{-- 有料選択 --}}
 <h3 class="tracking-widest mt-12 mb-4 text-[15px] font-semibold">{{ __('料金設定') }}</h3>
 <div class="checkbox">
-  <label class="light-checkbox">
-    <input type="checkbox" name="is_free"
-      @if ($update) {{ !$e->is_free ?? old('is_free') ? 'checked' : '' }} @endif
-      class="light-checkbox-Input">
-    <span class="light-checkbox-DummyInput">
-      <svg width="10" height="8" class="stroke-white" viewBox="0 0 10 8" fill="none">
-        <path d="M0.75 3.99998L3.58 6.82998L9.25 1.16998" stroke-width="1.5" stroke-linecap="round"
-          stroke-linejoin="round" />
-      </svg>
-    </span>
-    <span class="light-checkbox-LabelText">{{ __('このエピソードを有料で販売する') }}</span>
-  </label>
+  @empty(!$book->user->stripe_user_id)
+    <label class="light-checkbox">
+      <input type="checkbox" name="is_free"
+        @if ($update) {{ !$e->is_free ?? old('is_free') ? 'checked' : '' }} @endif
+        class="light-checkbox-Input">
+      <span class="light-checkbox-DummyInput">
+        <svg width="10" height="8" class="stroke-white" viewBox="0 0 10 8" fill="none">
+          <path d="M0.75 3.99998L3.58 6.82998L9.25 1.16998" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        </svg>
+      </span>
+      <span class="light-checkbox-LabelText">{{ __('このエピソードを有料で販売する') }}</span>
+    </label>
 
-  @if ($update && !$e->is_free)
-    <p class="mt-4 tracking-widest">現在の値段：<strong>{{ $e->price ?? old('price') }}</strong>{{ __('円') }}
-    </p>
-  @endif
+    @if ($update && !$e->is_free)
+      <p class="mt-4 tracking-widest">現在の値段：<strong>
+          {{ $e->price ?? old('price') }}</strong>{{ __('円') }}
+      </p>
+    @endif
+  @endempty
 </div>
+
+@empty($book->user->stripe_user_id)
+  <p class="mt-4 mb-2 bg-red bg-opacity-10 text-red px-4 py-2 font-semibold">
+    {{ __('有料販売をするには設定の「収益を受け取る準備」が完了している必要があります') }}
+  </p>
+@endempty
 
 {{-- ご注意点 --}}
 <h3 class="tracking-widest mt-12 mb-4 text-[15px] font-semibold">{{ __('ご注意点') }}</h3>
