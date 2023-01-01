5355ef985d74539f82ce4e4e8864990229e6e066
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-01 17:57:58 +0900

    やはり戻す

--- app/Http/Controllers/Books/Episode/StoreController.php
+++ app/Http/Controllers/Books/Episode/StoreController.php
@@ -138,7 +138,6 @@ class StoreController extends Controller
             'id' => 'prod_' . $episode->id,
             'name' => $book->title . ' - ' . $episode->number . '話',
             'default_price_data' => [
-                'id' => 'price_' . $episode->id,
                 'unit_amount' => 50, // デフォルトは50円
                 'currency' => 'jpy',
                 'tax_behavior' => 'inclusive', // 内税
