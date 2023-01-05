1a42f3f043a30e8a2e7201295994ccf8564d6858
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-05 21:22:33 +0900

    ロゴ更新

--- resources/views/books/episode/show.blade.php
+++ resources/views/books/episode/show.blade.php
@@ -1,71 +1,92 @@
 @extends('app')
 
 @php
-    $a = __('エピソード');
-    $b = __('作品情報');
-    $c = __('コメント');
-    $d = __('件');
+  $a = __('エピソード');
+  $b = __('作品情報');
+  $c = __('コメント');
+  $d = __('件');
 @endphp
 
 @section('title', $episode->number . __('話') . ' - ' . $book->title)
 
 @isset($episode->thumbnail)
-    @section('image')
-        <meta property="og:image" content="{{ $episode->thumbnail }}">
-        <meta property="og:image:secure_url" content="{{ $episode->thumbnail }}">
-        <meta name="twitter:image" content="{{ $episode->thumbnail }}">
-    @endsection
+  @section('image')
+    <meta property="og:image" content="{{ $episode->thumbnail }}">
+    <meta property="og:image:secure_url" content="{{ $episode->thumbnail }}">
+    <meta name="twitter:image" content="{{ $episode->thumbnail }}">
+  @endsection
 @endisset
 
 @isset($book->story)
-    @section('description')
-        <meta name="description" itemprop="description" content="{{ $book->story }}">
-        <meta property="og:description" content="{{ $book->story }}">
-        <meta name="twitter:description" content="{{ $book->story }}">
-    @endsection
+  @section('description')
+    <meta name="description" itemprop="description" content="{{ $book->story }}">
+    <meta property="og:description" content="{{ $book->story }}">
+    <meta name="twitter:description" content="{{ $book->story }}">
+  @endsection
 @endisset
 
 @section('content')
-    <div class="hidden lg:block">
-        @include('atoms._episode_nav', ['tab' => 0])
-    </div>
-    <div class="block lg:hidden">
-        @include('atoms._nav', ['tab' => 1])
-    </div>
+  <div class="hidden lg:block">
+    @include('atoms._episode_nav', ['tab' => 0])
+  </div>
+  <div class="block lg:hidden">
+    @include('atoms._nav', ['tab' => 1])
+  </div>
 
-    {{-- エピソードスクリーン --}}
-    <episode-screen :title='@json($book->title)' :episode-number='@json($episode->number)'
-        :contents='@json($episode->contents ?? [])' endpoint="{{ url('/') }}">
-    </episode-screen>
+  {{-- エピソードスクリーン --}}
+  <episode-screen :title='@json($book->title)' :episode-number='@json($episode->number)'
+    :contents='@json($episode->contents ?? [])' endpoint="{{ url('/') }}">
+  </episode-screen>
 
-    <div class="w-full mt-8 lg:mt-auto h-full bg-white dark:bg-dark">
-        <div class="max-w-7xl mx-auto md:py-8 flex flex-col md:flex-row justify-between">
-            {{-- 左サイドバー --}}
-            <div class="hidden lg:block">
-                @include('books.atoms.leftSidebar', [
-                    'sns_title' => $episode->number . __('話') . ' - ' . $book->title,
-                ])
+  @if (!$episode->is_free)
+    @auth
+      @isset($book->user->stripe_user_id)
+        @if ($book->user->id !== Auth::user()->id)
+          <div class="overflow-hidden h-[80vh] bg-dark bg-opacity-90 w-full flex flex-col items-center justify-center">
+            <div class="text-3xl mt-4 tracking-widest text-white">
+              {{ $book->title }} {{ $episode->number }}{{ __('話') }}</div>
+            <div class="mt-8">
+
+              {{-- 決済 --}}
+              @include('atoms.stripe_script', [
+                  'book' => $book,
+              ])
+
             </div>
+          </div>
+        @endif
+      @endisset
+    @endauth
+  @endif
 
-            <div class="w-full flex py-8">
+  <div class="w-full mt-8 lg:mt-auto h-full bg-white dark:bg-dark">
+    <div class="max-w-7xl mx-auto md:py-8 flex flex-col md:flex-row justify-between">
+      {{-- 左サイドバー --}}
+      <div class="hidden lg:block">
+        @include('books.atoms.leftSidebar', [
+            'sns_title' => $episode->number . __('話') . ' - ' . $book->title,
+        ])
+      </div>
 
-                {{-- メインコンテンツ --}}
-                <div class="px-4 md:px-6 w-full lg:w-2/3">
-                    <book-tab :is-comment="true" :count='@json(count($comments) ?? 0)'
-                        :one='@json($a)' :two='@json($b)'
-                        :three='@json($c)' :four='@json($d)'>
-                        @include('books.episode.tab.1')
-                        @include('books.episode.tab.2')
-                        @include('books.episode.tab.3')
-                    </book-tab>
-                </div>
+      <div class="w-full flex py-8">
 
-
-                {{-- 右サイドバー --}}
-                <div class="mg:pl-4 lg:w-1/3"></div>
-            </div>
+        {{-- メインコンテンツ --}}
+        <div class="px-4 md:px-6 w-full lg:w-2/3">
+          <book-tab :is-comment="true" :count='@json(count($comments) ?? 0)' :one='@json($a)'
+            :two='@json($b)' :three='@json($c)'
+            :four='@json($d)'>
+            @include('books.episode.tab.1')
+            @include('books.episode.tab.2')
+            @include('books.episode.tab.3')
+          </book-tab>
         </div>
-    </div>
 
-    @include('atoms._footer')
+
+        {{-- 右サイドバー --}}
+        <div class="mg:pl-4 lg:w-1/3"></div>
+      </div>
+    </div>
+  </div>
+
+  @include('atoms._footer')
 @endsection
