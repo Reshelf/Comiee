5f4b9c7254775507e3a9e40a561a85758418324a
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-10 15:20:34 +0900

    httpsの強制

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        # 開発環境（local）はhttpなので、httpsにしない
-        if ($this->app->environment('production')) {
+        if ($this->app->environment() == 'production' || $this->app->environment() == 'staging') {
             \URL::forceScheme('https');
         }
     }
