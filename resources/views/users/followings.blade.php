e3ea5c7af23ac03e4d8bc0b16e60bd2dbb968f17
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-10 21:51:05 +0900

    プロキシの設定

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -3,25 +3,25 @@
 @section('title', $user->name . __('さんのフォロー'))
 
 @section('content')
-    @include('atoms._nav', ['tab' => 0])
-    <div class="">
-        @include('users.atoms.user', [
-            'mypage' => false,
-            'settings' => false,
-        ])
+  @include('atoms._nav', ['tab' => 0])
+  <div class="">
+    @include('users.atoms.user', [
+        'mypage' => false,
+        'settings' => false,
+    ])
+  </div>
+  <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
+    <div class="py-8 w-full">
+      <follow-modal :user-name='@json($user->name)'>
+        <template #header>{{ $user->name }}{{ __('さんのフォロー') }}</template>
+        @if ($followings->count())
+          @foreach ($followings as $person)
+            @include('users.atoms.person')
+          @endforeach
+        @else
+          <p>{{ __('フォローしている人はいません') }}</p>
+        @endif
+      </follow-modal>
     </div>
-    <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
-        <div class="py-8 w-full">
-            <follow-modal :user-name='@json($user->name)'>
-                <template #header>{{ $user->name }}{{ __('さんのフォロー') }}</template>
-                @if ($followings->count())
-                    @foreach ($followings as $person)
-                        @include('users.atoms.person')
-                    @endforeach
-                @else
-                    <p>{{ __('フォローしている人はいません') }}</p>
-                @endif
-            </follow-modal>
-        </div>
-    </div>
+  </div>
 @endsection
