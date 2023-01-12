5a4481931ffdb8c2a3c65366b9f806aeb9d63a57
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-12 21:47:33 +0900

    マイグレーションからSanctumは除外

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // マイグレーションからSanctumは除外
+        \Laravel\Sanctum\Sanctum::ignoreMigrations();
     }
 
     /**
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // 本番とテスト環境はhttpsを強制
         if ($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
         }
