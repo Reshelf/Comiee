ff56a45abfd7269c0c8207083fa719991f4664ee
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-14 16:31:32 +0900

    コメント追加

--- resources/views/atoms/meta.blade.php
+++ resources/views/atoms/meta.blade.php
@@ -37,7 +37,8 @@
 <meta property="og:image:height" content="630">
 <meta name="twitter:card" content="summary_large_image">
 
-
+{{-- Google Fonts --}}
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap"
+  rel="stylesheet">
