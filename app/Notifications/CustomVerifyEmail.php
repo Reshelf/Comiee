d39f68e308ff78a414aa059339a4dce3bc31c49e
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-16 22:43:30 +0900

    一旦これでいく

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -2,9 +2,12 @@
 
 namespace App\Notifications;
 
+use Carbon\Carbon;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
 
 class CustomVerifyEmail extends VerifyEmail
 {
@@ -16,12 +19,24 @@ class CustomVerifyEmail extends VerifyEmail
      */
     public function toMail($notifiable)
     {
-        $verificationUrl = $this->verificationUrl($notifiable);
+        // $verificationUrl = $this->verificationUrl($notifiable);
+
+// $verificationUrl = config('app.top_url') . '/email/verify/' . $notifiable->getKey() . '/' . sha1($notifiable->getEmailForVerification());
+        // $frontendUrl = config('app.top_url') . '/email/verify/';
+        $verifyUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
 
         return (new MailMessage)
             ->subject(Lang::get('Verify Email Address'))
             ->markdown('emails.verify_email', [
-                'verify_url' => $verificationUrl,
+                'verify_url' => $verifyUrl,
             ]);
+
     }
 }
