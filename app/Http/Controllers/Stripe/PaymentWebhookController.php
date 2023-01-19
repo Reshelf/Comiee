8db3bf794e7f3bdd403c5c1dd7b697d3c49119f2
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-19 22:58:10 +0900

    webhookテスト

--- app/Http/Controllers/Stripe/PaymentWebhookController.php
+++ app/Http/Controllers/Stripe/PaymentWebhookController.php
@@ -22,23 +22,18 @@ class PaymentWebhookController extends Controller
 
         // エンドポイントのシークレットキーを指定
         $endpoint_secret = config('app.stripe_endpoint_secret');
-        $payload = $request->getContent();
-        $sig_header = $request->header('stripe-signature');
-
+        $payload = file_get_contents("php://input");
+        $sig_header = $_SERVER["HTTP_STRIPE_SIGNATURE"];
         $event = null;
 
         try {
             // 送信されてきたリクエストの情報から、webhookイベントのチェック
-            \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
+            $event = \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
         } catch (\UnexpectedValueException$e) {
             return response()->json('Invalid payload', 400);
         } catch (\Stripe\Exception\SignatureVerificationException$e) {
             return response()->json('Invalid Signature', 400);
         }
-
-        $event = \Stripe\Webhook::constructEvent(
-            $payload, $sig_header, $endpoint_secret
-        );
 
         // イベントタイプが「checkout.session.completed」（Checkoutセッションの完了）の場合は、決済完了の処理を行う
         if ($event->type == 'checkout.session.completed') {
