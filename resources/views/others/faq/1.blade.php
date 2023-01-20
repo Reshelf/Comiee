85c7d88ffba787303f0744873dd665cd3f5a8336
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-01-20 23:16:44 +0900

    FAQ サービスについて 更新

--- resources/views/others/faq/1.blade.php
+++ resources/views/others/faq/1.blade.php
@@ -19,9 +19,9 @@ p-8 flex flex-col items-end">
 
     <div class="w-full md:w-[70%] p-8 md:py-8 md:pl-20 md:pr-48">
       <h2 class="text-3xl font-semibold tracking-widest">サービスについて</h2>
-      <span class="inline-block text-xs mt-3">2022/10/17</span>
+      <span class="inline-block mt-3">2021/1/20</span>
 
-      <div class="my-16">
+      <div class="my-16 text-base">
         Comieeの概要やご利用に関するご質問をまとめました。
 
         <div class="flex flex-col text-primary mt-2">
@@ -29,13 +29,12 @@ p-8 flex flex-col items-end">
           <a href="#2" class="my-2">・推奨環境を教えてください</a>
           <a href="#3" class="my-2">・会員登録は必須ですか？</a>
           <a href="#4" class="my-2">・無料で利用できますか？</a>
-          <a href="#5" class="my-2">・アプリ版とweb版の違いって？</a>
         </div>
       </div>
 
       <div class="my-16">
         <h3 id="1" class="text-2xl font-semibold tracking-widest">Comieeってどんなアプリ？</h3>
-        <p class="mt-4 leading-8">
+        <p class="mt-4 leading-8 text-base">
           Comieeは、誰もがマンガを投稿でき、マンガを楽しむことのできるプラットフォームです。<br>
           気になる作者を応援したり、自分で投稿して収入を得ることもできます。
         </p>
@@ -43,28 +42,23 @@ p-8 flex flex-col items-end">
 
       <div class="my-16">
         <h3 id="2" class="text-2xl font-semibold tracking-widest">推奨環境を教えてください</h3>
-        <p class="mt-4 leading-8">
-          以下の通りです。<br><br>
-          【パソコン（ブラウザ）】<br>
-          〈Windows〉<br>
-          ・OS：Windows10以上<br>
-          ・ブラウザ：Chrome最新/Edge最新<br>
-          〈Mac OS〉<br>
-          ・OS：★ <br>
-          ・ブラウザ：Safari最新/Chrome最新<br><br>
-          【スマートフォン（アプリ）】<br>
-          〈iPhone〉<br>
-          ・OS：iOS★以上<br>
-          ・ブラウザ：Safari最新/Chrome最新<br>
-          〈Android〉<br>
-          ・OS：Android★以上<br>
-          ・ブラウザ：Chrome最新
+        <p class="mt-4 leading-8 text-base">
+          推奨環境は以下の通りです。<br><br>
+          OS<br>
+          ・iPhone,iPad：iOS 13.3以降<br>
+          ・Windows：Windows 8.1以降<br>
+          ・Mac：Mac OS 10.13以降<br><br>
+          ブラウザ<br>
+          ・Chrome：最新版<br>
+          ・Safari：最新版<br>
+          ・Microsoft Edge：最新版<br>
+          ・Firefox：最新版
         </p>
       </div>
 
       <div class="my-16">
         <h3 id="3" class="text-2xl font-semibold tracking-widest">会員登録（アカウント作成）やログインは必須ですか？</h3>
-        <p class="mt-4 leading-8">
+        <p class="mt-4 leading-8 text-base">
           必須ではありません。会員登録やログインをしなくても、作品を読むことはできます。<br>
           ただしその場合、以下の機能が使えません。<br><br>
           ・作品への「いいね！」<br>
@@ -79,14 +73,8 @@ p-8 flex flex-col items-end">
 
       <div class="my-16">
         <h3 id="4" class="text-2xl font-semibold tracking-widest">無料で利用できますか？</h3>
-        <p class="mt-4 leading-8">
+        <p class="mt-4 leading-8 text-base">
           有料作品の閲覧以外は、無料で利用できます。
-        </p>
-      </div>
-
-      <div class="my-16">
-        <h3 id="5" class="text-2xl font-semibold tracking-widest">アプリ版とWeb版の違いって？</h3>
-        <p class="mt-4 leading-8">
         </p>
       </div>
     </div>
