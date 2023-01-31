aaa3e32380ba7f60a43f86d400ace86ced6129c3
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-01 01:17:12 +0900

    csrf修正

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'https: //www.comiee.one/stripe/webhook',
+        'https://www.comiee.one/stripe/webhook',
     ];
 }
