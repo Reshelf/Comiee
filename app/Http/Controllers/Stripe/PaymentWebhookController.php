50c388b006a8199ccae5335f6ec2acbd3497bcbf
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-02 01:14:36 +0900

    ファイル整理

--- app/Http/Controllers/Stripe/PaymentWebhookController.php
+++ app/Http/Controllers/Stripe/PaymentWebhookController.php
@@ -30,17 +30,17 @@ class PaymentWebhookController extends Controller
             $event = \Stripe\Webhook::constructEvent(
                 $request->getContent(), $sig_header, $endpoint_secret
             );
-
-            if ($event->type == 'checkout.session.completed') {
-                $session = $event->data->object;
-                $this->handleCompletedCheckoutSession($session);
-            }
-            return response()->json('ok', 200);
         } catch (\UnexpectedValueException$e) {
             return response()->json('Invalid payload', 400);
         } catch (\Stripe\Exception\SignatureVerificationException$e) {
             return response()->json('Invalid Signature', 400);
         }
+
+        if ($event->type == 'checkout.session.completed') {
+            $session = $event->data->object;
+            $this->handleCompletedCheckoutSession($session);
+        }
+        return response()->json('ok', 200);
     }
 
     /*
