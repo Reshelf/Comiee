e35907172a814d69c3bc047b80b55c39ba4e2c9f
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-07 21:44:21 +0900

    バグ解消

--- resources/views/search/atoms/_content.blade.php
+++ resources/views/search/atoms/_content.blade.php
@@ -1,3 +1,6 @@
+@php
+  $type = null;
+@endphp
 <div class="w-full flex flex-wrap justify-start">
   @isset($books)
     @foreach ($books as $book)
@@ -13,33 +16,33 @@
           {{-- ランキング15位まではラベルをつける --}}
           @if ($books->currentpage() === 1 && $loop->iteration <= 10 && $type === null)
             <span class="ranking-rabel">{{ $loop->iteration }}</span>
-          @endif
+          @endempty
           <span class="thumbnail-title">{{ $book->title }}</span>
-        </a>
+      </a>
 
-        @empty($type)
-          <div class="flex items-center">
+      @empty($type)
+        <div class="flex items-center">
 
-            {{-- お気に入り --}}
-            @include('books.atoms.likes')
+          {{-- お気に入り --}}
+          @include('books.atoms.likes')
 
-            {{-- 閲覧回数 --}}
-            <div class="flex items-center text-aaa pl-4">
-              <svg class="stroke-666 w-[20px] h-[20px]" viewBox="0 0 24 24" fill="none">
-                <path d="M16.5 9.5L12.3 13.7L10.7 11.3L7.5 14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
-                  stroke-linejoin="round" />
-                <path d="M14.5 9.5H16.5V11.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke-width="1.5"
-                  stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
+          {{-- 閲覧回数 --}}
+          <div class="flex items-center text-aaa pl-4">
+            <svg class="stroke-666 w-[20px] h-[20px]" viewBox="0 0 24 24" fill="none">
+              <path d="M16.5 9.5L12.3 13.7L10.7 11.3L7.5 14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                stroke-linejoin="round" />
+              <path d="M14.5 9.5H16.5V11.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke-width="1.5"
+                stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
 
-              <span class="pl-2">{{ number_format($book->views) }}{{ __('回') }}</span>
-            </div>
+            <span class="pl-2">{{ number_format($book->views) }}{{ __('回') }}</span>
           </div>
-        @endempty
-      </div>
-    @endforeach
-  @endisset
+        </div>
+      @endempty
+    </div>
+  @endforeach
+@endisset
 
-  @include('atoms.nomessage')
+@include('atoms.nomessage')
 </div>
