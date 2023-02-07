f2bf8f26b6e5f0876d901aecf35435e3aa6f59ae
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-08 01:58:12 +0900

    一旦ここまで

--- resources/views/search/atoms/_content.blade.php
+++ resources/views/search/atoms/_content.blade.php
@@ -24,20 +24,23 @@
         <div class="flex items-center">
 
           {{-- お気に入り --}}
-          @include('books.atoms.likes')
+          @isset($book->user->id)
+            @include('books.atoms.likes')
 
-          {{-- 閲覧回数 --}}
-          <div class="flex items-center text-aaa pl-4">
-            <svg class="stroke-666 w-[20px] h-[20px]" viewBox="0 0 24 24" fill="none">
-              <path d="M16.5 9.5L12.3 13.7L10.7 11.3L7.5 14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
-                stroke-linejoin="round" />
-              <path d="M14.5 9.5H16.5V11.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-              <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke-width="1.5"
-                stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
+            {{-- 閲覧回数 --}}
+            <div class="flex items-center text-aaa pl-4">
+              <svg class="stroke-666 w-[20px] h-[20px]" viewBox="0 0 24 24" fill="none">
+                <path d="M16.5 9.5L12.3 13.7L10.7 11.3L7.5 14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                  stroke-linejoin="round" />
+                <path d="M14.5 9.5H16.5V11.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke-width="1.5"
+                  stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
 
-            <span class="pl-2">{{ number_format($book->views) }}{{ __('回') }}</span>
-          </div>
+              <span class="pl-2">{{ number_format($book->views) }}{{ __('回') }}</span>
+            </div>
+          @endisset
+
         </div>
       @endempty
     </div>
