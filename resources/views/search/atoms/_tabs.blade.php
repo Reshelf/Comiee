3a3c6776f95d4027529727ac97a1928ab09d7fcd
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-09 23:39:27 +0900

    本棚修正

--- resources/views/search/atoms/_tabs.blade.php
+++ resources/views/search/atoms/_tabs.blade.php
@@ -6,11 +6,9 @@
   <a href="{{ route('todays_new', app()->getLocale()) }}"
     class="{{ $tab === 2 ? 'dark:text-ddd font-bold' : 'border-transparent hover:text-primary dark:hover:text-ddd hover:font-semibold hover:dark:border-dark' }} py-3 px-4">{{ __('今日の新作') }}</a>
   @auth
-    <a href="{{ route('user.shelf.like', app()->getLocale()) }}"
-      class="{{ $tab === 3 ? 'dark:text-ddd font-bold' : 'border-transparent hover:text-primary dark:hover:text-ddd hover:font-semibold hover:dark:border-dark' }} py-3 px-4">{{ __('本棚') }}</a>
-  @endauth
-  @auth
     <a href="{{ route('ranking', app()->getLocale()) }}"
       class="{{ $tab === 1 ? 'dark:text-ddd font-bold' : 'border-transparent hover:text-primary dark:hover:text-ddd hover:font-semibold hover:dark:border-dark' }} py-3 px-4">{{ __('ランキング') }}</a>
+    <a href="{{ route('user.shelf.like', app()->getLocale()) }}"
+      class="{{ $tab === 3 ? 'dark:text-ddd font-bold' : 'border-transparent hover:text-primary dark:hover:text-ddd hover:font-semibold hover:dark:border-dark' }} py-3 px-4">{{ __('本棚') }}</a>
   @endauth
 </div>
