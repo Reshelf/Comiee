4f375cb5091c9d7e4b1f6d740d119b2fc70c01ae
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-02-25 11:38:34 +0900

    バグ修正

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -3,25 +3,25 @@
 @section('title', $user->name . __('さんのフォロワー'))
 
 @section('content')
-    @include('atoms._nav', ['tab' => 0])
-    <div class="">
-        @include('users.atoms.user', [
-            'mypage' => false,
-            'settings' => false,
-        ])
+  @include('atoms._nav', ['tab' => 0])
+  <div class="">
+    @include('users.atoms.user', [
+        'work' => false,
+        'about' => false,
+    ])
+  </div>
+  <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
+    <div class="py-8 w-full">
+      <follow-modal :user-name='@json($user->name)'>
+        <template #header>{{ $user->name }} {{ __('さんのフォロワー') }}</template>
+        @if ($followers->count())
+          @foreach ($followers as $person)
+            @include('users.atoms.person')
+          @endforeach
+        @else
+          <p>{{ __('フォロワーはいません') }}</p>
+        @endif
+      </follow-modal>
     </div>
-    <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
-        <div class="py-8 w-full">
-            <follow-modal :user-name='@json($user->name)'>
-                <template #header>{{ $user->name }} {{ __('さんのフォロワー') }}</template>
-                @if ($followers->count())
-                    @foreach ($followers as $person)
-                        @include('users.atoms.person')
-                    @endforeach
-                @else
-                    <p>{{ __('フォロワーはいません') }}</p>
-                @endif
-            </follow-modal>
-        </div>
-    </div>
+  </div>
 @endsection
--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -6,8 +6,8 @@
   @include('atoms._nav', ['tab' => 0])
   <div class="">
     @include('users.atoms.user', [
-        'mypage' => false,
-        'settings' => false,
+        'work' => false,
+        'about' => false,
     ])
   </div>
   <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
