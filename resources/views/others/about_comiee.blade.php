68c1b7b58c9835392b8cbfe7e084e279f3e45aad
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-05 11:20:52 +0900

    meta追加

--- resources/views/others/about_comiee.blade.php
+++ resources/views/others/about_comiee.blade.php
@@ -1,11 +1,18 @@
 @extends('app')
 
 @section('title', __('Comieeについて'))
-
 @php
   $faq_number = 1;
   $update_date = '2023/3/5';
+  $content = 'Comieeは作者がマンガを無料でアップロード・公開をして、ユーザーがそのコンテンツを楽しんで応援できるオンラインマンガプラットフォームです。だれもがマンガを楽しんで続けられるよう、安心できる雰囲気や、多様性を大切にしています。';
 @endphp
+
+@section('description')
+  <meta name="description" itemprop="description" content="{{ $content }}">
+  <meta property="og:description" content="{{ $content }}">
+  <meta name="twitter:description" content="{{ $content }}">
+@endsection
+
 @section('content')
   @include('atoms._nav', ['tab' => 0])
   <div class="w-full flex flex-col md:flex-row">
