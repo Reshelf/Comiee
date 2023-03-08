7879c73691f73debb1950808c040937fced9cacb
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-08 23:23:55 +0900

    本棚修正

--- resources/views/users/shelf/index.blade.php
+++ resources/views/users/shelf/index.blade.php
@@ -18,11 +18,11 @@
           <ul class="tabMenu scroll-none sticky top-0 lg:h-[300px]">
             <h3 class="text-2xl font-semibold py-4 lg:pt-0 hidden lg:block">{{ __('本棚') }}</h3>
             <a href="{{ route('user.shelf.like', ['lang' => app()->getLocale()]) }}"
-              class="{{ $type === 'like' ? 'font-bold text-primary' : '' }} ">{{ __('お気に入り') }}</a>
+              class="{{ $type === 'like' ? 'font-bold text-primary dark:text-ddd' : '' }} ">{{ __('お気に入り') }}</a>
             <a href="{{ route('user.shelf.view', ['lang' => app()->getLocale()]) }}"
-              class="{{ $type === 'view_history' ? 'font-bold text-primary' : '' }} ">{{ __('閲覧履歴') }}</a>
+              class="{{ $type === 'view_history' ? 'font-bold text-primary dark:text-ddd' : '' }} ">{{ __('閲覧履歴') }}</a>
             <a href="{{ route('user.shelf.purchase', ['lang' => app()->getLocale()]) }}"
-              class="{{ $type === 'purchase_history' ? 'font-bold text-primary' : '' }} ">{{ __('購入履歴') }}</a>
+              class="{{ $type === 'purchase_history' ? 'font-bold text-primary dark:text-ddd' : '' }} ">{{ __('購入履歴') }}</a>
           </ul>
 
           <div class="tabContents">
