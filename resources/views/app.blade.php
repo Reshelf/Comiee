84e7af25286a56b0d31d5eadd26c11aabf143fed
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-08 23:49:32 +0900

    google analitics削除

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
 
 <head>
   @include('atoms.meta')
-  {{-- @includeWhen(env('GA_ENABLE'), 'atoms.google_analytics') --}}
   @yield('header-scripts')
   @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/common/atoms/common.js'])
 </head>
