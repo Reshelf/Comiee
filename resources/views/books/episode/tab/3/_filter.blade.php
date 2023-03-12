6c31fa125b422029abb60ec1d552e16cc2a15fb4
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-12 18:04:47 +0900

    スタイル修正

--- resources/views/books/episode/tab/3/_filter.blade.php
+++ resources/views/books/episode/tab/3/_filter.blade.php
@@ -1,7 +1,7 @@
 <input id="acd-check-{{ $comment_id }}" class="acd-check hidden" type="checkbox">
 <label class="acd-label inline-block" for="acd-check-{{ $comment_id }}">
   <div
-    class="inline-flex hover:text-primary dark:hover:text-666 text-666 dark:text-ccc stroke-666 hover:stroke-primary dark:stroke-eee items-center text-xs rounded-[5px] px-2 py-1.5 font-semibold cursor-pointer">
+    class="inline-flex hover:text-primary dark:hover:text-666 text-666 dark:text-f5 stroke-666 hover:stroke-primary dark:stroke-eee items-center text-xs rounded-[5px] px-2 py-1.5 font-semibold cursor-pointer">
     {{ __('件の返信を表示する') }}
   </div>
 </label>
