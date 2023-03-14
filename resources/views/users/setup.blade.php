48a04c2993296b90b9bbbb549ae72a9ce737d9a8
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-14 19:09:55 +0900

    ようこそ文

--- resources/views/users/setup.blade.php
+++ resources/views/users/setup.blade.php
@@ -13,7 +13,18 @@
   <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
     <div class="py-8 w-full">
       <setup-modal :user='@json($user)'>
-        <template #header>Comieeへようこそ！</template>
+        <template #header>Comieeへようこそ</template>
+        <div class="text-base">
+          <p>アカウント登録が正常に完了しました。</p>
+          <p class="mt-2">これからあなたのマンガ活動が始まります。</p>
+          <p class="mt-2">多くのマンガと出会い、輪を広げていきましょう！</p>
+          <p class="mt-4">Comiee（コミー）について知りたい方向けに役立つページ</p>
+          <p class="mt-2">
+            <a href="{{ route('others.about.comiee', app()->getLocale()) }}" class="text-primary">・Comieeについて</a>
+          </p>
+        </div>
+
+
       </setup-modal>
     </div>
   </div>
