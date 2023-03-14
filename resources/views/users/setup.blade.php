b17aba8bcd491d47561bd37587e19cf8ffeee1f3
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-14 18:52:09 +0900

    ようこそページに変えなくては

--- resources/views/users/setup.blade.php
+++ resources/views/users/setup.blade.php
@@ -13,20 +13,7 @@
   <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
     <div class="py-8 w-full">
       <setup-modal :user='@json($user)'>
-        <template #header>パーソナライズ</template>
-        <form method="POST"
-          action="{{ route('users.update', ['lang' => app()->getLocale(), 'username' => $user->username]) }}"
-          enctype="multipart/form-data" onsubmit="submit_btn()">
-          @csrf
-          @method('PATCH')
-          {{-- @include('users.atoms.form', ['setup' => true, 'setup_update' => false]) --}}
-          <div class="relative">
-            <button type="submit" class="submit_btn3 btn-primary w-full lg:py-4">
-              {{ __('設定する') }}
-              <span class="load loading"></span>
-            </button>
-          </div>
-        </form>
+        <template #header>Comieeへようこそ！</template>
       </setup-modal>
     </div>
   </div>
