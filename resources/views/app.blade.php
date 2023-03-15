4946a57e49ae2bd05d55a72f9a12b72c64797974
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-15 10:28:32 +0900

    アナリティクス追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   @include('atoms.meta')
+  @includeWhen(env('GA_ENABLE'), 'atoms.google_analytics')
   @yield('header-scripts')
   @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/common/atoms/common.js'])
 </head>
---
+++ resources/views/atoms/google_analytics.blade.php
@@ -0,0 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-R2R5224E52"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-R2R5224E52');
+</script>
