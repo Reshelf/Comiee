d9b0f0a5ea85aaa36e6c8b927266361b7f4273a0
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-17 11:02:02 +0900

    ここまでアップ

--- resources/views/atoms/_spnav.blade.php
+++ resources/views/atoms/_spnav.blade.php
@@ -1,6 +1,28 @@
 <div class="mobile-menu">
+
+  {{-- ホーム --}}
+  <a href="{{ url('/') }}"
+    class="{{ $tab === 0 ? 'stroke-primary dark:stroke-white text-primary font-semibold' : 'stroke-[#7c7c7c] dark:stroke-ddd' }} text-center">
+    <svg width="28" height="28" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M9.02 2.83998L3.63 7.03998C2.73 7.73998 2 9.22998 2 10.36V17.77C2 20.09 3.89 21.99 6.21 21.99H17.79C20.11 21.99 22 20.09 22 17.78V10.5C22 9.28998 21.19 7.73998 20.2 7.04998L14.02 2.71998C12.62 1.73998 10.37 1.78998 9.02 2.83998Z"
+        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M12 17.99V14.99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </a>
+
+  {{-- 検索 --}}
+  <a
+    class="{{ $tab === 3 ? 'stroke-primary dark:stroke-white text-primary font-semibold' : 'stroke-[#7c7c7c] dark:stroke-ddd' }} text-center">
+    <svg width="28" height="28" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>
+  </a>
+
+  {{-- 投稿 --}}
   @auth
-    <div class="absolute right-[20px] top-[-65px]">
+    <div>
       <create-modal>
         {{-- エラー文 --}}
         @include('atoms._error_card_list')
@@ -19,28 +41,9 @@
     </div>
   @endauth
 
-  {{-- ホーム --}}
-  <a href="{{ url('/') }}"
-    class="{{ $tab === 0 ? 'stroke-primary text-primary font-semibold' : 'stroke-[#7c7c7c]' }} w-1/4 text-center">
-    <svg width="28" height="28" viewBox="0 0 24 24" fill="none">
-      <path
-        d="M9.02 2.83998L3.63 7.03998C2.73 7.73998 2 9.22998 2 10.36V17.77C2 20.09 3.89 21.99 6.21 21.99H17.79C20.11 21.99 22 20.09 22 17.78V10.5C22 9.28998 21.19 7.73998 20.2 7.04998L14.02 2.71998C12.62 1.73998 10.37 1.78998 9.02 2.83998Z"
-        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M12 17.99V14.99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-  </a>
-
-  {{-- 検索 --}}
-  <a class="{{ $tab === 3 ? 'stroke-primary text-primary font-semibold' : 'stroke-[#7c7c7c]' }} w-1/4 text-center">
-    <svg width="28" height="28" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-    </svg>
-  </a>
-
   {{-- 本棚 --}}
   <a href="{{ route('search.like', app()->getLocale()) }}"
-    class="{{ $tab === 3 ? 'stroke-primary text-primary font-semibold' : 'stroke-[#7c7c7c]' }} w-1/4 text-center">
+    class="{{ $tab === 3 ? 'stroke-primary dark:stroke-white text-primary font-semibold' : 'stroke-[#7c7c7c] dark:stroke-ddd' }} text-center">
     <svg width="28" height="28" viewBox="0 0 24 24" fill="none">
       <path
         d="M22 16.7399V4.66994C22 3.46994 21.02 2.57994 19.83 2.67994H19.77C17.67 2.85994 14.48 3.92994 12.7 5.04994L12.53 5.15994C12.24 5.33994 11.76 5.33994 11.47 5.15994L11.22 5.00994C9.44 3.89994 6.26 2.83994 4.16 2.66994C2.97 2.56994 2 3.46994 2 4.65994V16.7399C2 17.6999 2.78 18.5999 3.74 18.7199L4.03 18.7599C6.2 19.0499 9.55 20.1499 11.47 21.1999L11.51 21.2199C11.78 21.3699 12.21 21.3699 12.47 21.2199C14.39 20.1599 17.75 19.0499 19.93 18.7599L20.26 18.7199C21.22 18.5999 22 17.6999 22 16.7399Z"
@@ -54,7 +57,7 @@
   {{-- プロフィール --}}
   <a @if (Auth::user()) href="{{ route('users.show', ['lang' => app()->getLocale(), 'username' => Auth::user()->username]) }}"
         @else href="{{ route('login', app()->getLocale()) }}" @endif
-    class="{{ $tab === 4 ? 'stroke-primary text-primary font-semibold' : 'stroke-[#7c7c7c]' }} w-1/4">
+    class="{{ $tab === 4 ? 'stroke-primary dark:stroke-white text-primary font-semibold' : 'stroke-[#7c7c7c] dark:stroke-ddd' }}">
     @empty($user->avatar)
       <svg class="rounded-full shadow object-cover w-[28px] h-[28px]" viewBox="0 0 42 42" fill="none">
         <rect width="42" height="42" rx="21" class="dark:fill-dark-1 fill-eee" />
