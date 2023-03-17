b84150f9ea858059b554fda77a8cf9e57b507a1d
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-17 18:25:03 +0900

    セットアップ画面

--- resources/views/users/setup.blade.php
+++ resources/views/users/setup.blade.php
@@ -13,17 +13,19 @@
   <div class="flex max-w-lg w-full mx-auto px-6 md:px-0 justify-center">
     <div class="py-8 w-full">
       <setup-modal :user='@json($user)'>
-        <template #header>Comieeへようこそ</template>
+        <template #header>{{ __('Comieeへようこそ') }}</template>
         <div class="text-base">
-          <p>アカウント登録が正常に完了しました。</p>
-          <p class="mt-2">これからあなたのマンガ活動が始まります。</p>
-          <p class="mt-2">多くのマンガと出会い、輪を広げていきましょう！</p>
-          <p class="mt-4">Comiee（コミー）について知りたい方向けに役立つページ</p>
-          <p class="mt-2">
-            <a href="{{ route('others.user_guide', app()->getLocale()) }}" class="text-primary">・ご利用ガイド</a><br>
-            <a href="{{ route('others.about.comiee', app()->getLocale()) }}" class="text-primary">・Comieeについて</a><br>
+          <p>{{ __('アカウント登録が正常に完了しました。') }}</p>
+          <p class="mt-2">{{ __('これからあなたのマンガ活動が始まります。') }}</p>
+          <p class="mt-2">{{ __('多くのマンガと出会い、輪を広げていきましょう！') }}</p>
+          <p class="mt-4">{{ __('Comiee（コミー）について知りたい方向けに役立つページ') }}</p>
+          <p class="mt-4">
+            <a href="{{ route('others.user_guide', app()->getLocale()) }}"
+              class="block text-primary dark:text-[#8ab4f8] mb-2">・ご利用ガイド</a>
+            <a href="{{ route('others.about.comiee', app()->getLocale()) }}"
+              class="block text-primary dark:text-[#8ab4f8] mb-2">・Comieeについて</a>
             <a href="{{ route('others.faq', ['lang' => app()->getLocale(), 'number' => 1]) }}"
-              class="text-primary">・よくあるご質問</a><br>
+              class="block text-primary dark:text-[#8ab4f8]">・よくあるご質問</a>
           </p>
         </div>
       </setup-modal>
