96ddaa897fc013c6d084798adeda9914bf5a7479
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-17 23:49:35 +0900

    修正

--- resources/views/atoms/_spnav.blade.php
+++ resources/views/atoms/_spnav.blade.php
@@ -13,14 +13,14 @@
   </a>
 
   {{-- 検索 --}}
-  <a
+  <span
     class="{{ $tab === 3 ? 'stroke-primary dark:stroke-white text-primary font-semibold' : 'stroke-[#7c7c7c] dark:stroke-ddd' }} text-center">
     <svg width="28" height="28" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
       <title>search</title>
       <path stroke-linecap="round" stroke-linejoin="round"
         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
     </svg>
-  </a>
+  </span>
 
   {{-- 投稿 --}}
   @auth
