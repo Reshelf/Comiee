9cddd0912e0ba417f3df3719d16ee4e285ed4b82
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-17 16:49:38 +0900

    いいね後のメールを1日キャッシュで管理

--- app/Http/Controllers/Books/LikeController.php
+++ app/Http/Controllers/Books/LikeController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Books;
 
 use App\Http\Controllers\Controller;
+use App\Mail\books\LikedBookMail;
 use App\Models\Book;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Mail;
-use App\Mail\books\LikedBookMail;
 
 class LikeController extends Controller
 {
@@ -21,22 +20,33 @@ class LikeController extends Controller
     |--------------------------------------------------------------------------
     | 作品をお気に入りに追加する
     |--------------------------------------------------------------------------
-    */
+     */
     public function __invoke($lang, Request $request, Book $book)
     {
         // 作者以外のユーザー
         if ($book->user->id !== $request->user()->id) {
+
+            // お気に入りに追加
             $book->likes()->detach($request->user()->id);
             $book->likes()->attach($request->user()->id);
 
+            $sessionKey = "liked_book_email_{$request->user()->id}_to_{$book->user->id}";
+            $emailCooldown = 1440; // 送信間隔を1日に設定
+            $now = \Carbon\Carbon::now();
+
+            $lastEmailSentAt = $request->session()->get($sessionKey);
+
             // お気に入りされたらメールを送る
-            if ($book->user->m_notice_3 === 1) {
+            if (!$lastEmailSentAt || $now->diffInMinutes($lastEmailSentAt) > $emailCooldown) {
                 $mailData = [
                     'send_user' => $request->user(),
                     'received_user' => $book->user,
-                    'book' => $book
+                    'book' => $book,
                 ];
                 Mail::send(new LikedBookMail($mailData));
+
+                // セッションに最後に送信されたメールの日時を保存
+                $request->session()->put($sessionKey, $now);
             }
         }
 
