675148ba0d4bfbd6eeec0f87a3e2cc5327f20884
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-20 16:27:28 +0900

    修正

--- resources/views/emails/books/liked.blade.php
+++ resources/views/emails/books/liked.blade.php
@@ -35,20 +35,31 @@
               <table border="0" width="100%" cellspacing="0" cellpadding="0" align="center"
                 style="border-collapse:collapse;max-width:300px;word-break:break-word">
                 <tbody>
-                  @empty(!$mailData['send_user']->avatar)
-                    <tr>
-                      <td width="80" align="center">
-                        <a href="{{ config('app.top_url') . '/' . app()->getLocale() . '/' . $mailData['send_user']->username }}"
-                          style="color:#1b74e4;text-decoration:none" target="_blank">
+                  <tr>
+                    <td width="80" align="center">
+                      <a href="{{ config('app.top_url') . '/' . app()->getLocale() . '/' . $mailData['send_user']->username }}"
+                        style="color:#1b74e4;text-decoration:none" target="_blank">
+                        @empty(!$mailData['send_user']->avatar)
                           <img width="80" height="80" src="{{ $mailData['send_user']->avatar }}"
                             style="border:solid 1px rgba(0,0,0,.15);border-radius:50%;object-fit:cover" loading="lazy">
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td height="7" style="line-height:7px"></td>
-                    </tr>
-                  @endempty
+                        @else
+                          <svg width="80" height="80" viewBox="0 0 42 42" fill="none">
+                            <title>user icon</title>
+                            <rect width="42" height="42" rx="21" fill="#ebebeb" />
+                            <path
+                              d="M21 21C23.7614 21 26 18.7614 26 16C26 13.2386 23.7614 11 21 11C18.2386 11 16 13.2386 16 16C16 18.7614 18.2386 21 21 21Z"
+                              stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M29.5901 31C29.5901 27.13 25.7402 24 21.0002 24C16.2602 24 12.4102 27.13 12.4102 31"
+                              stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                          </svg>
+                        @endempty
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="7" style="line-height:7px"></td>
+                  </tr>
+
                   <tr>
                     <td width="100%" align="center">
                       <a href="{{ config('app.top_url') . '/' . app()->getLocale() . '/' . $mailData['send_user']->username }}"
