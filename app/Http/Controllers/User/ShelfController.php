bbb9321c62e654cc68f7f14b9c68956f10f26d72
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-23 22:23:39 +0900

    未ログインでできることを増やす

--- app/Http/Controllers/User/ShelfController.php
+++ app/Http/Controllers/User/ShelfController.php
@@ -4,33 +4,28 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ShelfController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
-    }
-
     /*
     |--------------------------------------------------------------------------
     | 本棚
     |--------------------------------------------------------------------------
     |
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        $username = Auth::user()->username;
-        $user = User::byUsername($username)->first();
+        if (Auth::user()) {
+            $username = Auth::user()->username;
+            $user = User::byUsername($username)->first();
+        }
 
         return view('users.shelf.index', [
-            'user' => $user,
-            'likes' => $user->likes, // お気に入り
-            'reads' => $user->reads, // 閲覧履歴
-            'boughts' => $user->bought, // 購入履歴
+            'user' => $user ?? [],
+            'likes' => $user->likes ?? [], // お気に入り
+            'reads' => $user->reads ?? [], // 閲覧履歴
+            'boughts' => $user->bought ?? [], // 購入履歴
         ]);
     }
 }
--- resources/views/atoms/_spnav.blade.php
+++ resources/views/atoms/_spnav.blade.php
@@ -25,25 +25,22 @@
   </span>
 
   {{-- 投稿 --}}
-  @auth
-    <div>
-      <create-modal>
-        {{-- エラー文 --}}
-        @include('atoms._error_card_list')
-        @include('atoms.success')
-        <form method="POST" action="{{ route('book.store', app()->getLocale()) }}" enctype="multipart/form-data"
-          onsubmit="submit_btn()">
-          @include('books.atoms.form', ['update' => false, 'create_book_modal_count' => 14])
-          <div class="w-full relative">
-            <button type="submit" class="submit_btn3 btn-primary w-full lg:py-4">
-              {{ __('投稿する') }}
-              <span class="load loading"></span>
-            </button>
-          </div>
-        </form>
-      </create-modal>
-    </div>
-  @endauth
+  <div>
+    <create-modal>
+      @include('atoms._error_card_list')
+      @include('atoms.success')
+      <form method="POST" action="{{ route('book.store', app()->getLocale()) }}" enctype="multipart/form-data"
+        onsubmit="submit_btn()">
+        @include('books.atoms.form', ['update' => false, 'create_book_modal_count' => 14])
+        <div class="w-full relative">
+          <button type="submit" class="submit_btn3 btn-primary w-full lg:py-4">
+            {{ __('投稿する') }}
+            <span class="load loading"></span>
+          </button>
+        </div>
+      </form>
+    </create-modal>
+  </div>
 
   {{-- 本棚 --}}
   <a href="{{ route('user.shelf.like', app()->getLocale()) }}"
