89ae5294357ba9e1598382a4443f37d4aa6c012e
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-24 00:35:34 +0900

    修正

--- resources/views/auth/_login.blade.php
+++ resources/views/auth/_login.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('login') }}" class="hover:text-primary dark:hover:text-f5">ログイン</a>
+<a href="{{ route('login') }}" class="btn-primary">ログイン</a>
 <a href="{{ route('register') }}" class="ml-4 hover:text-primary dark:hover:text-f5">新規登録</a>
