6095d28421feb28d29dec706a2f664ce49cc6a24
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-03-24 23:02:06 +0900

    お知らせはコメントアウト

--- resources/views/atoms/_footer.blade.php
+++ resources/views/atoms/_footer.blade.php
@@ -154,8 +154,8 @@
       <div class="mt-8 w-full flex flex-col md:flex-row justify-between">
         <div class="md:w-1/2">
           <div class="tracking-widest text-xl mb-4 cursor-default dark:text-[#c9cacc]">{{ __('ニュース') }}</div>
-          <span
-            class="block mb-2 hover:text-primary dark:hover:text-ddd">{{ __('お知らせ') }}{{ __('(準備中)') }}</span>
+          {{-- <span
+            class="block mb-2 hover:text-primary dark:hover:text-ddd">{{ __('お知らせ') }}{{ __('(準備中)') }}</span> --}}
           <a href="{{ route('others.release_note', app()->getLocale()) }}"
             class="block mb-2 hover:text-primary dark:hover:text-ddd">{{ __('リリースノート') }}</a>
         </div>
