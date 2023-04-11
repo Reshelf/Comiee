66a69667e9ab3ebcf30c1fa22cd9941051eefa09
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-04-11 17:09:12 +0900

    バグ修正

--- app/Http/Controllers/Analytics/IndexController.php
+++ app/Http/Controllers/Analytics/IndexController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('signed')->only('verify');
+    }
+
     /*
     |--------------------------------------------------------------------------
     | 分析ページ
--- resources/js/components/analytics/page/ContentsDashboard.vue
+++ resources/js/components/analytics/page/ContentsDashboard.vue
@@ -10,7 +10,6 @@
             >
                 <div class="px-4 min-w-[382px]">作品</div>
                 <div class="px-4 min-w-[100px]">公開設定</div>
-                <div class="px-4 min-w-[120px] text-right">ページビュー数</div>
                 <div class="px-4 min-w-[100px] text-right">閲覧回数</div>
                 <div class="px-4 min-w-[100px] text-right">ユーザー数</div>
                 <div class="px-4 min-w-[100px] text-right">お気に入り数</div>
@@ -126,12 +125,6 @@
                     > -->
                 </div>
 
-                <!-- ページビュー -->
-                <div
-                    class="flex justify-end px-4 min-w-[120px] max-w-[120px] mb-2 pb-2 border-b border-comiee"
-                >
-                    {{ formatNumber(book.page_views.length) }}
-                </div>
                 <!-- 閲覧回数 -->
                 <div
                     class="flex justify-end px-4 min-w-[100px] max-w-[100px] mb-2 pb-2 border-b border-comiee"
