59d1403bd8b25ba6757272462255a296da91de14
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-04-17 09:26:13 +0900

    規約一部をコンポーネント化

--- resources/js/common/components.ts
+++ resources/js/common/components.ts
@@ -1,121 +1,124 @@
 import { defineAsyncComponent } from "vue";
 
-const BookLike = defineAsyncComponent(() =>
-    import("../components/books/BookLike.vue")
+const BookLike = defineAsyncComponent(
+    () => import("../components/books/BookLike.vue")
 );
-const BooksLists = defineAsyncComponent(() =>
-    import("../components/books/BooksLists.vue")
+const BooksLists = defineAsyncComponent(
+    () => import("../components/books/BooksLists.vue")
 );
-const PageViewsGraph = defineAsyncComponent(() =>
-    import("../components/analytics/book/PageViewsGraph.vue")
+const PageViewsGraph = defineAsyncComponent(
+    () => import("../components/analytics/book/PageViewsGraph.vue")
 );
-const BookEditModal = defineAsyncComponent(() =>
-    import("../components/books/BookEditModal.vue")
+const BookEditModal = defineAsyncComponent(
+    () => import("../components/books/BookEditModal.vue")
 );
-const CreateModal = defineAsyncComponent(() =>
-    import("../components/books/CreateModal.vue")
+const CreateModal = defineAsyncComponent(
+    () => import("../components/books/CreateModal.vue")
 );
-const DeleteModal = defineAsyncComponent(() =>
-    import("../components/books/DeleteModal.vue")
+const DeleteModal = defineAsyncComponent(
+    () => import("../components/books/DeleteModal.vue")
 );
-const EditModal = defineAsyncComponent(() =>
-    import("../components/books/EditModal.vue")
+const EditModal = defineAsyncComponent(
+    () => import("../components/books/EditModal.vue")
 );
-const CommentPostModal = defineAsyncComponent(() =>
-    import("../components/books/episodes/comments/CommentPostModal.vue")
+const CommentPostModal = defineAsyncComponent(
+    () => import("../components/books/episodes/comments/CommentPostModal.vue")
 );
-const EpisodeList = defineAsyncComponent(() =>
-    import("../components/books/episodes/EpisodeList.vue")
+const EpisodeList = defineAsyncComponent(
+    () => import("../components/books/episodes/EpisodeList.vue")
 );
-const EpisodeScreen = defineAsyncComponent(() =>
-    import("../components/books/episodes/EpisodeScreen.vue")
+const EpisodeScreen = defineAsyncComponent(
+    () => import("../components/books/episodes/EpisodeScreen.vue")
 );
-const BookTagsInput = defineAsyncComponent(() =>
-    import("../components/BookTagsInput.vue")
+const BookTagsInput = defineAsyncComponent(
+    () => import("../components/BookTagsInput.vue")
 );
-const CountAnimation = defineAsyncComponent(() =>
-    import("../components/CountAnimation.vue")
+const CountAnimation = defineAsyncComponent(
+    () => import("../components/CountAnimation.vue")
 );
-const FollowButton = defineAsyncComponent(() =>
-    import("../components/FollowButton.vue")
+const FollowButton = defineAsyncComponent(
+    () => import("../components/FollowButton.vue")
 );
-const HeaderUserModal = defineAsyncComponent(() =>
-    import("../components/HeaderUserModal.vue")
+const HeaderUserModal = defineAsyncComponent(
+    () => import("../components/HeaderUserModal.vue")
 );
-const SearchForm = defineAsyncComponent(() =>
-    import("../components/search/SearchForm.vue")
+const SearchForm = defineAsyncComponent(
+    () => import("../components/search/SearchForm.vue")
 );
-const SpSearchForm = defineAsyncComponent(() =>
-    import("../components/search/SpSearchForm.vue")
+const SpSearchForm = defineAsyncComponent(
+    () => import("../components/search/SpSearchForm.vue")
 );
-const TagSearchModal = defineAsyncComponent(() =>
-    import("../components/search/TagSearchModal.vue")
+const TagSearchModal = defineAsyncComponent(
+    () => import("../components/search/TagSearchModal.vue")
 );
-const ThemeToggle = defineAsyncComponent(() =>
-    import("../components/ThemeToggle.vue")
+const ThemeToggle = defineAsyncComponent(
+    () => import("../components/ThemeToggle.vue")
 );
-const AvatarZoom = defineAsyncComponent(() =>
-    import("../components/users/AvatarZoom.vue")
+const AvatarZoom = defineAsyncComponent(
+    () => import("../components/users/AvatarZoom.vue")
 );
-const EditUserModal = defineAsyncComponent(() =>
-    import("../components/users/EditUserModal.vue")
+const EditUserModal = defineAsyncComponent(
+    () => import("../components/users/EditUserModal.vue")
 );
-const FollowModal = defineAsyncComponent(() =>
-    import("../components/users/FollowModal.vue")
+const FollowModal = defineAsyncComponent(
+    () => import("../components/users/FollowModal.vue")
 );
-const SetupModal = defineAsyncComponent(() =>
-    import("../components/users/SetupModal.vue")
+const SetupModal = defineAsyncComponent(
+    () => import("../components/users/SetupModal.vue")
 );
-const ShelfLists = defineAsyncComponent(() =>
-    import("../components/users/ShelfLists.vue")
+const ShelfLists = defineAsyncComponent(
+    () => import("../components/users/ShelfLists.vue")
 );
-const ThumbnailZoom = defineAsyncComponent(() =>
-    import("../components/users/ThumbnailZoom.vue")
+const ThumbnailZoom = defineAsyncComponent(
+    () => import("../components/users/ThumbnailZoom.vue")
 );
-const WorksLists = defineAsyncComponent(() =>
-    import("../components/users/WorksLists.vue")
+const WorksLists = defineAsyncComponent(
+    () => import("../components/users/WorksLists.vue")
 );
-const CommentLike = defineAsyncComponent(() =>
-    import("../components/books/episodes/comments/CommentLike.vue")
+const CommentLike = defineAsyncComponent(
+    () => import("../components/books/episodes/comments/CommentLike.vue")
 );
-const EpisodeLike = defineAsyncComponent(() =>
-    import("../components/books/episodes/EpisodeLike.vue")
+const EpisodeLike = defineAsyncComponent(
+    () => import("../components/books/episodes/EpisodeLike.vue")
 );
-const ReleaseNote = defineAsyncComponent(() =>
-    import("../components/ReleaseNote.vue")
+const ReleaseNote = defineAsyncComponent(
+    () => import("../components/ReleaseNote.vue")
 );
-const GetUserInfo = defineAsyncComponent(() =>
-    import("../components/analytics/atoms/GetUserInfo.vue")
+const GetUserInfo = defineAsyncComponent(
+    () => import("../components/analytics/atoms/GetUserInfo.vue")
 );
-const AnalyticsDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/AnalyticsDashboard.vue")
+const AnalyticsDashboard = defineAsyncComponent(
+    () => import("../components/analytics/AnalyticsDashboard.vue")
 );
-const CommentsDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/CommentsDashboard.vue")
+const CommentsDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/CommentsDashboard.vue")
 );
-const TopDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/TopDashboard.vue")
+const TopDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/TopDashboard.vue")
 );
-const ContentsDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/ContentsDashboard.vue")
+const ContentsDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/ContentsDashboard.vue")
 );
-const RankingDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/RankingDashboard.vue")
+const RankingDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/RankingDashboard.vue")
 );
-const UserDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/UserDashboard.vue")
+const UserDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/UserDashboard.vue")
 );
-const TrendDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/TrendDashboard.vue")
+const TrendDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/TrendDashboard.vue")
 );
-const SalesDashboard = defineAsyncComponent(() =>
-    import("../components/analytics/page/SalesDashboard.vue")
+const SalesDashboard = defineAsyncComponent(
+    () => import("../components/analytics/page/SalesDashboard.vue")
 );
-const BounceRateTracker = defineAsyncComponent(() =>
-    import("../components/analytics/atoms/BounceRateTracker.vue")
+const BounceRateTracker = defineAsyncComponent(
+    () => import("../components/analytics/atoms/BounceRateTracker.vue")
 );
-
+const TermsOfService = defineAsyncComponent(
+    () => import("../components/others/TermsOfService.vue")
+);
 export default {
+    TermsOfService,
     BounceRateTracker,
     SalesDashboard,
     TopDashboard,
