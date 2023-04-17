45acfb692d0f5de166e9d5fe8d6ef117fdd6a9dd
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-04-17 15:55:07 +0900

    エピソードの型定義

---
+++ resources/js/types/episode.ts
@@ -0,0 +1,15 @@
+export interface Episode {
+    id: number;
+    book_id: number;
+    number: number | null;
+    title: string | null;
+    contents: any[] | null;
+    thumbnail: string | null;
+    short_from_author: string | null;
+    is_hidden: boolean;
+    is_free: boolean;
+    price: number;
+    views: number;
+    created_at: string;
+    updated_at: string;
+}
