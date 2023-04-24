b96cc44162c3ccec5ab5e2b584712ebfc4f5a947
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-04-24 13:38:04 +0900

    修正

--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -2,5 +2,9 @@
 @section('content')
   @include('atoms._nav', ['tab' => 0])
 
-  <analytics-dashboard :user="{{ $user }}" :books="{{ $books }}"></analytics-dashboard>
+  @if ($books->count() > 0)
+    <analytics-dashboard :user="{{ $user }}" :books="{{ $books }}"></analytics-dashboard>
+  @else
+    <div class="flex items-center justify-center p-12">{{ __('分析する作品がありません') }}</div>
+  @endif
 @endsection
